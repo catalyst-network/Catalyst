bf6c7a04311f09321866ec2f0c4b412e609f5a4d
James Kirkby <jkirkby@protonmail.ch>
2018-11-16 08:52:25 +0000

    bootstrap economic engine

---
+++ Node/Economics/DFed.cs
@@ -0,0 +1,18 @@
+using ADL.Node.Interfaces;
+
+namespace ADL.Node.Economics
+{
+    /// <summary>
+    /// DMF - Decentralised Monetary Fund.
+    /// </summary>
+    internal class DMF : IDFed
+    {        
+        /// <summary>
+        /// DMF constructor.
+        /// </summary>
+        internal DMF()
+        {
+
+        }
+    }
+}
