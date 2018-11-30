2936a68610aa18718e0d4c7d65621d19cde4451a
James Kirkby <james.kirby@atlascity.io>
2018-11-30 15:22:15 +0000

    update

--- Tools/gen_protos.sh
+++ Tools/gen_protos.sh
@@ -13,12 +13,12 @@ readonly generated_dir=${greeter_protos_dir}/Server
 mkdir -p ${generated_dir}
 
 find ${greeter_protos_dir} -type f -name '*.proto' | \
-    xargs %{} ${proto_tools}/protoc {} \
+    xargs -J{} ${proto_tools}/protoc {} \
       --csharp_out ${generated_dir} \
       --proto_path ${greeter_protos_dir}
 
 find ${greeter_protos_dir} -type f -name '*.proto' | \
-    xargs %{} ${proto_tools}/protoc {} \
+    xargs -J{} ${proto_tools}/protoc {} \
       --proto_path ${greeter_protos_dir} \
       --grpc_out ${generated_dir} \
       --plugin=protoc-gen-grpc=${proto_tools}/grpc_csharp_plugin
