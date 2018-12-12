73634d607df7de6933c621c6963c741bafbab133
James Kirkby <jkirkby@protonmail.ch>
2018-12-12 09:09:52 +0000

    make sure log folder is not part of ignore rule

---
+++ Helpers/Logger/Log.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ADL.Log
+{
+    public class Log
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void Message(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
