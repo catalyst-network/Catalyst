64ca6b500b9ca22e9a0e99100f7dd14b9263d35e
James Kirkby <jkirkby@protonmail.ch>
2018-12-28 14:06:01 +0000

    refactor peerIdentifier algorithim

--- Node/Core/Modules/Network/Peer/PeerIdentifier.cs
+++ Node/Core/Modules/Network/Peer/PeerIdentifier.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net;
 using System.Security.Cryptography;
 using System.Text;
+using System.Reflection;
 
 namespace ADL.Node.Core.Modules.Network.Peer
 {
@@ -11,7 +12,7 @@ namespace ADL.Node.Core.Modules.Network.Peer
     public class PeerIdentifier
     { 
         public long Nonce { set; get; }
-        public byte[] NodeId { set; get; }
+        public byte[] PeerId { set; get; }
         private byte[] PublicKey { get; set; }
         private IPEndPoint EndPoint { get; set; }
 
@@ -22,40 +23,76 @@ namespace ADL.Node.Core.Modules.Network.Peer
         /// <param name="endPoint"></param>
         public PeerIdentifier(byte[] publicKey, IPEndPoint endPoint)
         {
+            // we need a public key with at least 20 bytes anything else wont do.
+            if (publicKey.Length < 20)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
             PublicKey = publicKey;
             EndPoint = endPoint;
-            NodeId = GenerateNodeId();
+            PeerId = BuildPeerId();
         }
 
         /// <summary>
-        /// Node ID's should return a unsigned 42 byte array in the following format, to produce a 336 bit key space
-        /// the ip chunk is 16 bytes long to account for ipv6 addresses, ipv4 addresses are only 4bytes long, in case of ipv4 the leading 12 bytes should all be 0x0 
-        /// clientID [2] + clientVersion[4] + Ip[16] + Port[2] + random[18]
+        /// Peer ID's should return a unsigned 42 byte array in the following format, to produce a 336 bit key space
+        /// the ip chunk is 16 bytes long to account for ipv6 addresses, ipv4 addresses are only 4bytes long, in case of ipv4 the leading 12 bytes should be padded 0x0 
+        /// clientID [2] + clientVersion[2] + Ip[16] + Port[2] + pub[20]
         /// The client ID for this implementation is "AC" or hexadecimal 4143
         /// </summary>
         /// <returns></returns>
-        private byte[] GenerateNodeId()
+        private byte[] BuildPeerId()
         {
-            byte[] nodeId = new byte[42];
+            // init blank nodeId
+            byte[] peerId = new byte[42];
+            
+            // copy client id chunk
+            Buffer.BlockCopy(BuildClientIdChunk(), 0, peerId, 0, 2);
+            
+            // copy client version chunk
+            Buffer.BlockCopy(BuildClientVersionChunk(), 0, peerId, 2, 2);
+            
+            // copy client ip chunk
+            Buffer.BlockCopy(BuildClientIpChunk(), 0, peerId, 4, 16);
 
-            byte[] clientIdChunk = Encoding.ASCII.GetBytes("AC");
+            // copy client port chunk
+            Buffer.BlockCopy(BuildClientPortChunk(), 0, peerId, 20, 2);
+      
+            // copy client public key chunk
+            Buffer.BlockCopy(PublicKey, 0, peerId, 22, 20);
             
-            Buffer.BlockCopy(clientIdChunk, 0, nodeId, 0, 2);
+            // log if we are debugging
+            Log.Log.Message(BitConverter.ToString(peerId));
             
-            string[] versionString = "1.12.24".Split('.'); //@TODO get assembly version
+            return peerId;
+        }
+
+        private byte[] BuildClientIdChunk()
+        {
+            return Encoding.ASCII.GetBytes("AC");
+        }
+
+        /// <summary>
+        /// We only care about the major ass string! 🍑 🍑 🍑 
+        /// </summary>
+        /// <returns></returns>
+        private byte[] BuildClientVersionChunk()
+        {
+            Version assVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            string majorAssString = assVersion.Major.ToString();
             
-            for (int i = 0; i < versionString.Length; i++)
+            if (majorAssString.Length < 2)
             {
-                if (versionString[i].Length < 2)
-                {
-                    versionString[i] = versionString[i].PadLeft(2, '0');
-                }
+                majorAssString = majorAssString.PadLeft(2, '0');
             }
                             
-            var versionBytes = Encoding.ASCII.GetBytes(string.Join("", versionString).Substring(0, 4));
-            Buffer.BlockCopy(versionBytes, 0, nodeId, 2, 2);
+            return Encoding.ASCII.GetBytes(majorAssString);
+        }
+
+        private byte[] BuildClientIpChunk()
+        {
             byte[] ipChunk = new byte[16];
-            IPAddress address = IPAddress.Parse("2001:0db8:85a3:0000:0000:8a2e:0370:7334");
+            IPAddress address = IPAddress.Parse(ADL.Network.Ip.GetPublicIP());
             byte[] ipBytes = address.GetAddressBytes();
             
             if(ipBytes.Length == 4)
@@ -67,23 +104,20 @@ namespace ADL.Node.Core.Modules.Network.Peer
             {
                 ipChunk = ipBytes;
             }
-            
-            Buffer.BlockCopy(ipChunk, 0, nodeId, 4, 16);
-            int port = 65535;
+
+            return ipChunk;
+        }
+
+        private byte[] BuildClientPortChunk()
+        {
+            int port = 65535; //@TODO get this from our bind endpoint
             byte[] portBytes = Encoding.ASCII.GetBytes(port.ToString("X"));
             byte[] portChunk = new byte[2];
             if (portBytes.Length > 4)
             {
-                
+               //@TODO pad with 0x0 so we always have same length.
             }
-            Buffer.BlockCopy(Encoding.ASCII.GetBytes(port.ToString("X")), 0, nodeId, 20, 2);
-      
-            var returnArray = new byte[20];
-            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-            rng.GetBytes(returnArray);
-            Buffer.BlockCopy(returnArray, 0, nodeId, 22, 20);
-            
-            Console.WriteLine(BitConverter.ToString(nodeId));
+            return Encoding.ASCII.GetBytes(port.ToString("X"));
         }
     }
 }
