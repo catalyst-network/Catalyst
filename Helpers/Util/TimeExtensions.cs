b97b73a363d996156a168629597590680d64d9f2
James Kirkby <james.kirby@atlascity.io>
2019-01-07 09:37:31 +0000

    update

--- Helpers/Util/TimeExtensions.cs
+++ Helpers/Util/TimeExtensions.cs
@@ -4,13 +4,36 @@ namespace ADL.Util
 {
     static class TimeExtensions
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
+        internal static TimeSpan Minutes(this int seconds)
+        {
+            if (seconds <= 0) throw new ArgumentOutOfRangeException(nameof(seconds));
+            return TimeSpan.FromMinutes(seconds);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
         internal static TimeSpan Seconds(this int seconds)
         {
+            if (seconds <= 0) throw new ArgumentOutOfRangeException(nameof(seconds));
             return TimeSpan.FromSeconds(seconds);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="milliseconds"></param>
+        /// <returns></returns>
         internal static TimeSpan Milliseconds(this int milliseconds)
         {
+            if (milliseconds <= 0) throw new ArgumentOutOfRangeException(nameof(milliseconds));
             return TimeSpan.FromMilliseconds(milliseconds);
         }
     }
