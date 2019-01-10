85770fc84a9c60a4c4c81cd45f4298a02674722a
James Kirkby <james.kirby@atlascity.io>
2019-01-10 21:44:23 +0000

    remove SeedServer to its own repo

--- SeedServer/hexUtil.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    decodeHex: (hexx) => {
-        var hex = hexx.toString();
-        var str = '';
-        for (var i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
-            str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-        return str;
-    }
-}
