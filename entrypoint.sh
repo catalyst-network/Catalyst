2a1ddbe7692dc81406e58a6764349c362c137334
James Kirkby <james.kirby@atlascity.io>
2019-02-04 08:01:40 +0000

    remove unneeded files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 PUBLIC_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
-dotnet Catalyst.Node.dll --public-key $PUBLIC_KEY --payout-address jw8dh3ns92p2msjs73jdnga --disable-dfs --disable-rpc -d
+PAYOUT_ADDRESS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
+dotnet Catalyst.Node.dll --public-key $PUBLIC_KEY --payout-address $PAYOUT_ADDRESS --disable-dfs --disable-rpc -d
