89642a0070799a9d5834f96d8bb8ca7f5259cfbd
nshCore <nshcore@protonmail.com>
2019-03-19 07:58:18 -0700

    Ipfs connector (#251)
    * resolve ipfs into dfs
    
    * resolve ipfs into dfs
    
    * working
    
    * update ipfsconnector wrapper
    
    * Merge IDfs, IpfsEngine.ICoreApi, and PeerTalk.IService
    
    * Use max length on password reader
    
    * code quality -- remove the badge when this drops 🤔😂
    
    * codacy IDispose ..

---
+++ src/Catalyst.Node.Common/Interfaces/Modules/Dfs/IIpfsDfs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Ipfs;
+using Ipfs.CoreApi;
+using Ipfs.Engine;
+using PeerTalk;
+
+namespace Catalyst.Node.Common.Interfaces.Modules.Dfs
+{
+    public interface IIpfsDfs : IDfs, ICoreApi, IService, IDisposable
+    {
+    }
+
+    
+}
