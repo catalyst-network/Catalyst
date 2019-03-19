91fd601f7f1506ef38d2d85278e30db634eba5c5
Sherif <sherif@atlascity.io>
2019-03-19 15:19:59 +0000

    Merge branch '248-RPC-Module' of https://github.com/catalyst-network/Catalyst.Node into 248-RPC-Module
    # Conflicts:
    #	src/Catalyst.Node.Core/Config/devnet.json

--- src/Catalyst.Node.Common/Interfaces/IRpcClient.cs
+++ src/Catalyst.Node.Common/Interfaces/IRpcClient.cs
@@ -1,9 +1,12 @@
 using System.Threading.Tasks;
+using Catalyst.Node.Common.Helpers.Shell;
 
 namespace Catalyst.Node.Common.Interfaces
 {
     public interface IRpcClient
     {
          Task RunClientAsync();
+         
+         Task RunClientAsync(RpcNode node);
     }
 }
