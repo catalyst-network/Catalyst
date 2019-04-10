db29d331d130b71fa1a3e496774d99a8a7cabec2
Sherif <sherif@atlascity.io>
2019-04-10 16:31:27 +0100

    fixing build issues.

--- src/Catalyst.Node.Core/RPC/RpcServer.cs
+++ src/Catalyst.Node.Core/RPC/RpcServer.cs
@@ -8,12 +8,12 @@
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 2 of the License, or
 * (at your option) any later version.
-*
+* 
 * Catalyst.Node is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
-*
+* 
 * You should have received a copy of the GNU General Public License
 * along with Catalyst.Node. If not, see <https://www.gnu.org/licenses/>.
 */
@@ -68,7 +68,7 @@ namespace Catalyst.Node.Core.RPC
             var longRunningTasks = new [] {StartServerAsync()};
 
             _infoRequestHandler = new GetInfoRequestHandler(MessageStream, Settings, logger);
-            _versionRequestHandler = new GetVersionRequestHandler(MessageStream, Settings, logger);
+            _versionRequestHandler = new GetVersionRequestHandler(MessageStream, logger);
             _mempoolRequestHandler = new GetMempoolRequestHandler(MessageStream, logger, mempool);
             _signMessageRequestHandler = new SignMessageRequestHandler(MessageStream, logger, keySigner);
 
