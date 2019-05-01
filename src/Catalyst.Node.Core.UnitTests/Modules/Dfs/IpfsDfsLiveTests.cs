486d45b1d10bb3d3531a2f761b84099eecc1d5e3
Richard Schneider <makaretu@gmail.com>
2019-05-01 12:22:10 +1200

    test(IpfsDfs): use the real IPFS engine

---
+++ src/Catalyst.Node.Core.UnitTests/Modules/Dfs/IpfsDfsLiveTests.cs
@@ -0,0 +1,94 @@
+#region LICENSE
+
+/**
+* Copyright (c) 2019 Catalyst Network
+*
+* This file is part of Catalyst.Node <https://github.com/catalyst-network/Catalyst.Node>
+*
+* Catalyst.Node is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Catalyst.Node is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Catalyst.Node. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Catalyst.Common.Extensions;
+using Catalyst.Node.Core.Modules.Dfs;
+using FluentAssertions;
+using Serilog;
+using NSubstitute;
+using Xunit;
+using Xunit.Abstractions;
+using Catalyst.Common.UnitTests.TestUtils;
+using Catalyst.Common.Interfaces.P2P;
+using Catalyst.Common.Interfaces.Cryptography;
+
+namespace Catalyst.Node.Core.UnitTest.Modules.Dfs
+{
+    public sealed class IpfsDfsLiveTests : FileSystemBasedTest
+    {
+        private readonly IIpfsEngine _ipfsEngine;
+        private readonly ILogger _logger;
+        private readonly IPeerSettings _peerSettings;
+        private readonly IPasswordReader _passwordReader;
+
+        public IpfsDfsLiveTests(ITestOutputHelper output) : base(output)
+        {
+            _peerSettings = Substitute.For<IPeerSettings>();
+            _peerSettings.SeedServers.Returns(new[] { "seed1.server.va", "island.domain.tv" });
+            _passwordReader = Substitute.For<IPasswordReader>();
+            _passwordReader.ReadSecurePassword().ReturnsForAnyArgs(TestPasswordReader.BuildSecureStringPassword("abcd"));
+            _logger = Substitute.For<ILogger>();
+            _ipfsEngine = new IpfsEngine(_passwordReader, _peerSettings, FileSystem, _logger);
+        }
+
+        [Fact]
+        public async Task DFS_should_add_and_read_text()
+        {
+            const string text = "good morning";
+            var dfs = new IpfsDfs(_ipfsEngine, _logger);
+            var id = await dfs.AddTextAsync(text);
+            var content = await dfs.ReadTextAsync(id);
+            content.Should().Be(text);
+        }
+
+        [Fact]
+        public async Task DFS_should_add_and_read_binary()
+        {
+            var binary = new byte[] { 1, 2, 3 };
+            var ms = new MemoryStream(binary);
+            var dfs = new IpfsDfs(_ipfsEngine, _logger);
+            var id = await dfs.AddAsync(ms);
+            using (var stream = await dfs.ReadAsync(id))
+            {
+                var content = new byte[binary.Length];
+                stream.Read(content, 0, content.Length);
+                content.Should().Equal(binary);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _ipfsEngine?.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
