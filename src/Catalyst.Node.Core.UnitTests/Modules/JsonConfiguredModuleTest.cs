0e81c2aa8babaabb68c587801e77d78e16698144
Millymanz <dennis565@yahoo.co.uk>
2019-05-15 12:02:35 +0100

    376 store entity of the account state in an i repository (#380)
    * Created Account class with basic member variables
    
    * Implemented the ability to add account state to ledger repository
    
    * change account type from decimal to a BigDecimal

--- src/Catalyst.Node.Core.UnitTests/Modules/JsonConfiguredModuleTest.cs
+++ src/Catalyst.Node.Core.UnitTests/Modules/JsonConfiguredModuleTest.cs
@@ -34,10 +34,7 @@ using Catalyst.Common.Interfaces.Modules.Ledger;
 using Catalyst.Common.Interfaces.Modules.Mempool;
 using Catalyst.Common.Interfaces.P2P;
 using Catalyst.Common.UnitTests.TestUtils;
-using Catalyst.Node.Core.Modules.Consensus;
 using Catalyst.Node.Core.Modules.Contract;
-using Catalyst.Node.Core.Modules.Dfs;
-using Catalyst.Node.Core.Modules.Ledger;
 using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using NSubstitute;
@@ -85,7 +82,7 @@ namespace Catalyst.Node.Core.UnitTest.Modules
         [InlineData(typeof(IConsensus), typeof(Core.Modules.Consensus.Consensus))]
         [InlineData(typeof(IContract), typeof(Contract))]
         [InlineData(typeof(IDfs), typeof(Core.Modules.Dfs.Dfs))]
-        [InlineData(typeof(ILedger), typeof(Ledger))]
+        [InlineData(typeof(ILedger), typeof(Core.Modules.Ledger.Ledger))]
         [InlineData(typeof(IMempool), typeof(Core.Modules.Mempool.Mempool))]
         [Trait(Traits.TestType, Traits.IntegrationTest)]
         private void ComponentsJsonFile_should_configure_modules(Type interfaceType, Type resolutionType)
