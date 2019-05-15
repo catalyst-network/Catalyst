a967344daf9c4d8323b5a5c047aa6aa07d09c8ca
franssl <46971650+franssl@users.noreply.github.com>
2019-05-15 13:39:40 +0100

    Replace NSec functionality with rust library (#386)
    * add Rust.Cryptography.FFI.Wrapper submodule and create RustCryptoContext
    
    * Update namespace names
    
    * updated Catalyst.Cryptography.BulletProofs.Wrapper package to one including Interfaces folder

--- src/Catalyst.Common/Interfaces/Cryptography/ICryptoContext.cs
+++ src/Catalyst.Common/Interfaces/Cryptography/ICryptoContext.cs
@@ -22,7 +22,7 @@
 #endregion
 
 using System;
-
+using Catalyst.Cryptography.BulletProofs.Wrapper.Interfaces;
 namespace Catalyst.Common.Interfaces.Cryptography
 {
     public interface ICryptoContext
@@ -67,9 +67,9 @@ namespace Catalyst.Common.Interfaces.Cryptography
         /// <param name="privateKey"></param>
         /// <param name="data"></param>
         /// <returns></returns>
-        byte[] Sign(IPrivateKey privateKey, ReadOnlySpan<byte> data);
+        ISignature Sign(IPrivateKey privateKey, ReadOnlySpan<byte> data);
 
-        bool Verify(IPublicKey key, ReadOnlySpan<byte> data, ReadOnlySpan<byte> signature);
+        bool Verify(IPublicKey key, ReadOnlySpan<byte> data, ISignature signature);
 
         /// <summary>
         ///     Given a private key returns corresponding public key.
--- src/Catalyst.Common/Keystore/LocalKeyStore.cs
+++ src/Catalyst.Common/Keystore/LocalKeyStore.cs
@@ -24,6 +24,7 @@
 using System;
 using System.IO;
 using Catalyst.Common.Interfaces.Cryptography;
+using Catalyst.Cryptography.BulletProofs.Wrapper.Interfaces;
 using Catalyst.Common.Interfaces.FileSystem;
 using Catalyst.Common.Interfaces.KeyStore;
 using Serilog;
