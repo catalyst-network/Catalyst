535d0cc7b838230d8e9fa80f7d66dfe65dc90eed
Millymanz <dennis565@yahoo.co.uk>
2019-05-21 17:25:36 +0100

    377 node i want to build a delta for account state updates (#407)
    * Implemented the ability to add account state to ledger repository
    
    * Implemented requested changes from the discussion on Account properties and more
    
    * Changed Balance type to BigInteger
    
    * New submodule update
    
    * Refactored code and wrote tests for ledger state update builder
    
    * Updated validity criteria
    
    * cs-multibase deleted
    
    * Redundant code stubs removed
    
    * Update IDeltaBuilder.cs
    
    * Update ILedger.cs

---
+++ src/Catalyst.Node.Core.UnitTests/Modules/Consensus/DeltaBuilderTests.cs
@@ -0,0 +1,131 @@
+#region LICENSE
+
+/**
+* Copyright (c) 2019 Catalyst Network
+*
+* This file is part of Catalyst.Node <https://github.com/catalyst-network/Catalyst.Node>
+*
+* Catalyst.Node is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Catalyst.Node is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Catalyst.Node. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Catalyst.Common.Interfaces.Modules.Mempool;
+using Catalyst.Common.UnitTests.TestUtils;
+using Catalyst.Node.Core.Modules.Consensus;
+using Catalyst.Protocol.Transaction;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+using Google.Protobuf;
+using Multiformats.Hash;
+using Multiformats.Hash.Algorithms;
+
+namespace Catalyst.Node.Core.UnitTest.Modules.Consensus
+{
+    public sealed class DeltaBuilderTests
+    {
+        private readonly IMempool _mempool;
+
+        public DeltaBuilderTests()
+        {
+            var random = new Random();
+
+            _mempool = Substitute.For<IMempool>();
+            var transactions = Enumerable.Range(0, 20).Select(i =>
+            {
+                var transaction = TransactionHelper.GetTransaction(
+                    version: (uint) i,
+                    transactionFees: (ulong) random.Next(),
+                    timeStamp: (ulong) random.Next(),
+                    signature: i.ToString(),
+                    lockTime: 0);
+                return transaction;
+            }).ToList();
+
+            _mempool.GetMemPoolContent().Returns(transactions);
+        }
+
+        [Fact]
+        public void BuildDeltaEmptyPoolContent()
+        {
+            var mempool = Substitute.For<IMempool>();
+            mempool.GetMemPoolContent().Returns(new List<Transaction>());
+
+            var deltaBuilder = new DeltaBuilder(mempool, PeerIdentifierHelper.GetPeerIdentifier("testvalue"),
+                Encoding.UTF8.GetBytes("kUox886YuiZojgogjtgo83pkUox886YuiZ"));
+
+            var deltaEntity = deltaBuilder.BuildDelta();
+            deltaEntity.Should().Be(DeltaEntity.Default); 
+        }
+
+        [Fact]
+        public void BuildDeltaInvalidTransactionsBasedOnLockTime()
+        {
+            var random = new Random(12);
+
+            var invalidtransactionList = Enumerable.Range(0, 20).Select(i =>
+            {
+                var transaction = TransactionHelper.GetTransaction(
+                    version: (uint) i,
+                    transactionFees: 954,
+                    timeStamp: 157,
+                    signature: i.ToString(),
+                    lockTime: (ulong) random.Next() + 475);
+                return transaction;
+            }).ToList();
+
+            var mempool = Substitute.For<IMempool>();
+            mempool.GetMemPoolContent().Returns(invalidtransactionList);
+
+            var deltaBuilder = new DeltaBuilder(mempool, PeerIdentifierHelper.GetPeerIdentifier("testvalue"),
+                Encoding.UTF8.GetBytes("kUox886YuiZojgogjtgo83pkUox886YuiZ"));
+
+            var deltaEntity = deltaBuilder.BuildDelta();
+            deltaEntity.Should().Be(DeltaEntity.Default);
+        }
+
+        [Fact]
+        public void BuildDeltaCheckForAccuracy()
+        {
+            //Transactions are faked and will always return results
+            //Safe to get first
+            var transactionSignature = DeltaBuilder.GetValidTransactionsForDelta(_mempool.GetMemPoolContent())
+               .First().Signature.ToByteArray();
+
+            var publicKeySeed = "randomseedm";
+            var peerId = PeerIdentifierHelper.GetPeerIdentifier(publicKeySeed);
+            var previousLedgerStateUpdate = "bkJsrbzIbuWm8EPSjJ2YicTIe5gIfEdfhXJK7dl7ESkjhDWUxkUox886YuiZnhEj3om5AXmWVcXvfzIAw";
+
+            var preLedgerStateUpdateByteArray = Encoding.UTF8.GetBytes(previousLedgerStateUpdate);
+
+            var deltaBuilder = new DeltaBuilder(_mempool, peerId, preLedgerStateUpdateByteArray);
+            var deltaEntity = deltaBuilder.BuildDelta();
+
+            deltaEntity.Should().NotBeNull();
+
+            Encoding.UTF8.GetString(deltaEntity.LocalLedgerState).Should().Contain(publicKeySeed);
+
+            deltaEntity.LocalLedgerState.Should().EndWith(deltaEntity.LocalLedgerState.TakeLast(preLedgerStateUpdateByteArray.Length));
+
+            deltaEntity.Delta.Should().EndWith(transactionSignature);
+
+            deltaEntity.DeltaHash.Should().ContainInOrder(Multihash.Encode<BLAKE2B_256>(deltaEntity.Delta));
+        }
+    }
+}
---
+++ src/Catalyst.Node.Core/Modules/Consensus/DeltaBuilder.cs
@@ -0,0 +1,142 @@
+#region LICENSE
+
+/**
+* Copyright (c) 2019 Catalyst Network
+*
+* This file is part of Catalyst.Node <https://github.com/catalyst-network/Catalyst.Node>
+*
+* Catalyst.Node is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Catalyst.Node is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Catalyst.Node. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System.Collections.Generic;
+using System.Linq;
+using Catalyst.Common.Interfaces.Modules.Consensus;
+using Catalyst.Common.Interfaces.Modules.Mempool;
+using Catalyst.Protocol.Transaction;
+using Multiformats.Hash;
+using Multiformats.Hash.Algorithms;
+using Nethereum.Hex.HexConvertors.Extensions;
+using Catalyst.Common.Extensions;
+using Catalyst.Common.Util;
+using Google.Protobuf;
+using Catalyst.Common.Interfaces.P2P;
+using System;
+using System.Text;
+using Nethereum.RLP;
+using Catalyst.Common.Config;
+using Dawn;
+
+namespace Catalyst.Node.Core.Modules.Consensus
+{
+    /// <inheritdoc />
+    public class DeltaBuilder : IDeltaBuilder
+    {
+        private readonly IMempool _mempool;
+
+        private readonly IPeerIdentifier _producerUniqueId;
+
+        private readonly byte[] _previousValidLedgerStateUpdate;
+
+        public DeltaBuilder(IMempool mempool, IPeerIdentifier producerUniqueId, byte[] previousValidLedgerStateUpdate)
+        {
+            Guard.Argument(mempool, nameof(mempool)).NotNull();
+            Guard.Argument(producerUniqueId, nameof(producerUniqueId)).NotNull();
+
+            _mempool = mempool;
+            _producerUniqueId = producerUniqueId;
+            _previousValidLedgerStateUpdate = previousValidLedgerStateUpdate;
+        }
+
+        ///<inheritdoc />
+        public IDeltaEntity BuildDelta()
+        {
+            var allTransactions = _mempool.GetMemPoolContent();
+            Guard.Argument(allTransactions, nameof(allTransactions))
+               .NotNull("Mempool content returned null, check the mempool is actively running");
+
+            var allValidatedTransactions = GetValidTransactionsForDelta(allTransactions);
+
+            if (allValidatedTransactions.Any())
+            {
+                var transactionSignature = allValidatedTransactions.First().Signature;
+                var selectedSTEntries = allValidatedTransactions.SelectMany(ste => ste.STEntries).ToList();
+
+                //Sorted O1< O2< ... < Oβ< ... < OM
+                var transationEntryListLexiOrder = SortHashByLexiOrder(selectedSTEntries);
+
+                //h∆j = blake2b256(∆Ln,j)
+                return CreateDeltaEntity(transationEntryListLexiOrder, transactionSignature);
+            }
+
+            return DeltaEntity.Default;
+        }
+
+        private DeltaEntity CreateDeltaEntity(List<STTransactionEntry> transationEntryListLexiOrder, TransactionSignature transactionSignature)
+        {
+            //L(f/E)
+            var transationEntryListByteArray = transationEntryListLexiOrder.SelectMany(lo => lo.ToByteArray()).ToArray();
+
+            //∆Ln,j = L(f/E) + dn
+            var deltaState = ByteUtil.CombineByteArrays(transationEntryListByteArray, transactionSignature.ToByteArray());
+            
+            //hj = h∆j + Idj
+            var localHash = Multihash.Encode<BLAKE2B_256>(deltaState);
+
+            //wj = hj + Idj
+            var localLedgerStateUpdate = ByteUtil.CombineByteArrays(localHash, _producerUniqueId.PeerId.ToByteArray());
+
+            return new DeltaEntity {LocalLedgerState = localLedgerStateUpdate, Delta = deltaState, DeltaHash = localHash};
+        }
+
+        private List<STTransactionEntry> SortHashByLexiOrder(List<STTransactionEntry> selectedSTEntries)
+        {
+            return selectedSTEntries.OrderBy(CreateTransactionEntryHash).ToList();
+        }
+
+        private byte[] CreateTransactionEntryHash(STTransactionEntry transEntry)
+        {
+            var transEntryByte = transEntry.ToByteArray();
+
+            //HEX(Eα)
+            var hexTransactionEntry = HexByteConvertorExtensions.ToHex(transEntryByte);
+
+            var seed = Multihash.Encode<BLAKE2B_256>(RLP.EncodeElement(_previousValidLedgerStateUpdate));
+
+            var merkleSeedInt = BitConverter.ToInt32(seed.Take(Constants.MerkleTreeFirstStandardBits).ToArray(), 0);
+            var random = new Random(merkleSeedInt);
+   
+            //s + HEX(Eα)
+            var saltHexConcat = Encoding.UTF8.GetBytes(string.Concat(random.Next().ToString(), hexTransactionEntry));
+
+            // Oα = blake2b256[s + HEX(Eα)]
+            var transEntryHash = Multihash.Encode<BLAKE2B_256>(saltHexConcat);
+            return transEntryHash;
+        }
+
+        /// <summary>
+        /// Gets the valid transactions for delta.
+        /// This method can be used to extract the collection of transactions that meet the criteria for validating delta.
+        /// </summary>
+        /// <param name="allTransactions">All transactions.</param>
+        /// <returns></returns>
+        public static IList<Transaction> GetValidTransactionsForDelta(IEnumerable<Transaction> allTransactions)
+        {
+            //lock time equals 0 or less than ledger cycle time
+            //we assume all transactions are of type non-confidential for now
+            return allTransactions.Where(m => m.LockTime <= 0 && m.Version == 1).ToList();
+        }
+    }
+}
