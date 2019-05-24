91efb19da2dc468271c26f0fe389a82278f06023
Sanjay Patel <sanjay.patel@atlascity.io>
2019-05-24 10:40:28 +0100

    Change transaction to transaction broadcast

--- src/Catalyst.Common.UnitTests/ProtobufExtensionsTests.cs
+++ src/Catalyst.Common.UnitTests/ProtobufExtensionsTests.cs
@@ -37,8 +37,8 @@ namespace Catalyst.Common.UnitTests
         [Fact]
         public static void ShortenedFullName_should_remove_namespace_start()
         {
-            TransactionBroadcast.Descriptor.FullName.Should().Be("Catalyst.Protocol.Transaction.Transaction");
-            TransactionBroadcast.Descriptor.ShortenedFullName().Should().Be("Transaction.Transaction");
+            TransactionBroadcast.Descriptor.FullName.Should().Be("Catalyst.Protocol.TransactionBroadcast.");
+            TransactionBroadcast.Descriptor.ShortenedFullName().Should().Be("Transaction.TransactionBroadcast");
         }
 
         [Fact]
