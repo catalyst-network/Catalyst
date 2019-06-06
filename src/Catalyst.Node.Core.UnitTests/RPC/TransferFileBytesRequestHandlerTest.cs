dce07d6cd3da15f353d274c772fb1d6f9ba009b3
nshcore <nshcore@protonmail.com>
2019-06-06 16:17:26 +0100

    WIP

--- src/Catalyst.Node.Core.UnitTests/RPC/TransferFileBytesRequestHandlerTest.cs
+++ src/Catalyst.Node.Core.UnitTests/RPC/TransferFileBytesRequestHandlerTest.cs
@@ -29,6 +29,7 @@ using Catalyst.Common.Extensions;
 using Catalyst.Common.Interfaces.FileTransfer;
 using Catalyst.Common.IO.Messaging;
 using Catalyst.Common.UnitTests.TestUtils;
+using Catalyst.Node.Core.RPC.Handlers;
 using Catalyst.Protocol.Common;
 using Catalyst.Protocol.Rpc.Node;
 using DotNetty.Transport.Channels;
