7be7945def7b922e00cc3772c42bf266ac267b7e
Richard Schneider <makaretu@gmail.com>
2019-06-11 02:40:01 +1200

    feat: browsing a DFS web site (#454)
    * There is now an optional module called DfsHttp, which provides HTTP access to the DFS.
    
    * Merge latest develop and fix
    
    * dfs gateway host on different port

--- src/Catalyst.Common/Interfaces/Modules/Dfs/IDfsHttp.cs
+++ src/Catalyst.Common/Interfaces/Modules/Dfs/IDfsHttp.cs
@@ -21,28 +21,25 @@
 
 #endregion
 
-using System.Collections.Generic;
-using System.Linq;
-using Catalyst.Common.Enumerator;
-using Catalyst.Common.Modules;
-using FluentAssertions;
-using Xunit;
+using Ipfs.HttpGateway;
 
-namespace Catalyst.Common.UnitTests.Modules
+namespace Catalyst.Common.Interfaces.Modules.Dfs
 {
-    public static class ModuleNamesTests
+    /// <summary>
+    ///   Provides read-only access to the distribute files system via HTTP.
+    /// </summary>
+    /// <seealso cref="IDfs"/>
+    public interface IDfsHttp
     {
-        [Fact]
-        public static void All_should_return_all_declared_names()
-        {
-            var allModuleNames = Enumeration.GetAll<ModuleName>().Select(m => m.Name);
-
-            var expectedList = new List<string>
-            {
-                "Consensus", "Contract", "Dfs", "DfsHttp", "Ledger", "Mempool", "KeySigner"
-            };
-
-            allModuleNames.Should().BeEquivalentTo(expectedList);
-        }
+        GatewayHost Gateway { get; }
+        
+        /// <summary>
+        ///   Gets the URL of DFS content.
+        /// </summary>
+        /// <param name="id">The unique ID of the content in the DFS.</param>
+        /// <returns>
+        ///   The URL of the DFS <paramref name="id"/>.
+        /// </returns>
+        string ContentUrl(string id);
     }
 }
