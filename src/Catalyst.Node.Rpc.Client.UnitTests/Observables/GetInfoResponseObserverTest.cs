90f7e6ee54a379d1e65695a69c9e9ae8efaf7efe
Sanjay Patel <sanjay.patel@atlascity.io>
2019-06-19 16:04:12 +0100

    Fix RpcClient_Can_Handle_GetInfoResponse

--- src/Catalyst.Node.Rpc.Client.UnitTests/Observables/GetInfoResponseObserverTest.cs
+++ src/Catalyst.Node.Rpc.Client.UnitTests/Observables/GetInfoResponseObserverTest.cs
@@ -21,24 +21,17 @@
 
 #endregion
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reactive.Linq;
 using Catalyst.Common.Config;
 using Catalyst.Common.Interfaces.Cli;
 using Catalyst.Common.IO.Messaging;
 using Catalyst.Common.IO.Messaging.Dto;
 using Catalyst.Node.Rpc.Client.Observables;
-using Catalyst.Protocol.Common;
 using Catalyst.Protocol.Rpc.Node;
 using Catalyst.TestUtils;
 using DotNetty.Transport.Channels;
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
 using NSubstitute;
 using Serilog;
+using System;
 using Xunit;
 
 namespace Catalyst.Node.Rpc.Client.UnitTests.Observables
@@ -47,30 +40,8 @@ namespace Catalyst.Node.Rpc.Client.UnitTests.Observables
     {
         private readonly ILogger _logger;
         private GetInfoResponseObserver _requestObserver;
-
-        private static readonly List<object[]> QueryContents;
         private readonly IChannelHandlerContext _fakeContext;
         private readonly IUserOutput _output;
-
-        static GetInfoResponseObserverTest()
-        {
-            var config = new ConfigurationBuilder()
-               .AddJsonFile(Path.Combine(Constants.ConfigSubFolder, Constants.ShellComponentsJsonConfigFile))
-               .AddJsonFile(Path.Combine(Constants.ConfigSubFolder, Constants.SerilogJsonConfigFile))
-               .AddJsonFile(Path.Combine(Constants.ConfigSubFolder, Constants.ShellNodesConfigFile))
-               .Build();
-
-            var configurationRoot = config.GetChildren().ToList().First();
-
-            var query = JsonConvert.SerializeObject(configurationRoot.AsEnumerable(),
-                Formatting.Indented);
-
-            QueryContents = new List<object[]>
-            {
-                new object[] {query},
-                new object[] {""}
-            };
-        }
 
         public GetInfoResponseObserverTest()
         {
@@ -79,36 +50,25 @@ namespace Catalyst.Node.Rpc.Client.UnitTests.Observables
             _output = Substitute.For<IUserOutput>();
         }
 
-        private IObservable<ProtocolMessageDto> CreateStreamWithMessage(ProtocolMessage response)
-        {
-            var channeledAny = new ProtocolMessageDto(_fakeContext, response);
-            var messageStream = new[]
-            {
-                channeledAny
-            }.ToObservable();
-            return messageStream;
-        }
-
-        [Theory(Skip = "This doesn't make sense")]
-        [MemberData(nameof(QueryContents))]
+        [Theory]
+        [InlineData("Test")]
+        [InlineData("Q2")]
         public void RpcClient_Can_Handle_GetInfoResponse(string query)
         {
+            var getInfoResponse = new GetInfoResponse
+            {
+                Query = query
+            };
             var response = new ProtocolMessageFactory().GetMessage(new MessageDto(
-                    new GetInfoResponse
-                    {
-                        Query = query
-                    },
+                    getInfoResponse,
                     MessageTypes.Response,
                     PeerIdentifierHelper.GetPeerIdentifier("recipient"),
                     PeerIdentifierHelper.GetPeerIdentifier("sender")
                 ),
                 Guid.NewGuid());
 
-            var messageStream = CreateStreamWithMessage(response);
-
             _requestObserver = new GetInfoResponseObserver(_output, _logger);
-            _requestObserver.StartObserving(messageStream);
-
+            _requestObserver.HandleResponse(new ProtocolMessageDto(_fakeContext, response));
             _output.Received(1).WriteLine(query);
         }
 
