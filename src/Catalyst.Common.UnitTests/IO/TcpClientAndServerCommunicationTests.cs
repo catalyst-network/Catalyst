b54c97cbfbaa80c9cf04f9effac514d6e0417140
monsieurleberre <monsieur.leberre@gmail.com>
2019-06-19 11:28:13 +0100

    more commented lines as test guidelines I am sure there are mistakes but the idea is here

--- src/Catalyst.Common.UnitTests/IO/TcpClientAndServerCommunicationTests.cs
+++ src/Catalyst.Common.UnitTests/IO/TcpClientAndServerCommunicationTests.cs
@@ -27,16 +27,21 @@ using System.Net;
 using System.Threading.Tasks;
 using Catalyst.Common.Extensions;
 using Catalyst.Common.Interfaces.IO.Messaging;
+using Catalyst.Common.Interfaces.IO.Messaging.Dto;
 using Catalyst.Common.Interfaces.Modules.KeySigner;
 using Catalyst.Common.Interfaces.P2P;
+using Catalyst.Common.Interfaces.P2P.Messaging.Dto;
 using Catalyst.Common.IO.Handlers;
 using Catalyst.Common.P2P;
+using Catalyst.Cryptography.BulletProofs.Wrapper.Interfaces;
 using Catalyst.Node.Core.RPC.Observables;
 using Catalyst.Protocol.Common;
 using Catalyst.Protocol.Rpc.Node;
 using Catalyst.TestUtils;
+using DotNetty.Common.Utilities;
 using DotNetty.Transport.Channels.Embedded;
 using FluentAssertions;
+using Google.Protobuf;
 using NSubstitute;
 using Serilog;
 using SharpRepository.Repository;
@@ -44,13 +49,16 @@ using Xunit;
 
 namespace Catalyst.Common.UnitTests.IO
 {
-    public class TcpClientAndServerCommunicationTests
+    public class TcpClientAndServerCommunicationTests : IDisposable
     {
         private readonly IMessageCorrelationManager _serverCorrelationManager;
         private readonly IKeySigner _serverKeySigner;
         private readonly TcpServerChannelFactoryTests.TestTcpServerChannelFactory _serverFactory;
         private readonly IKeySigner _clientKeySigner;
         private readonly TcpClientChannelFactoryTests.TestTcpClientChannelFactory _clientFactory;
+        private IRepository<Peer> _peerRepository;
+        private PeerCountRequestObserver _peerCountObserver;
+        private TestMessageObserver<GetPeerCountResponse> _clientObserver;
 
         public TcpClientAndServerCommunicationTests()
         {
@@ -83,29 +91,63 @@ namespace Catalyst.Common.UnitTests.IO
             var initialRequest = new GetPeerCountRequest().ToProtocolMessage(clientId, guid);
             clientChannel.WriteOutbound(initialRequest);
 
-            var sent = clientChannel.ReadOutbound<ProtocolMessage>();
-            sent.CorrelationId.ToGuid().Should().Be(guid);
+            var sent = clientChannel.ReadOutbound<ProtocolMessageSigned>();
+            //sent.Signature.Should().NotBeNullOrEmpty();
+            //sent.Message.CorrelationId.ToGuid().Should().Be(guid);
+            //var unwrappedMessage = sent.Message.FromProtocolMessage<GetPeerCountRequest>();
+            //unwrappedMessage.Should().NotBeNull();
 
-            var serverObserver = new ProtocolMessageObserver(0, Substitute.For<ILogger>());
-            var messageStream = _serverFactory.InheritedHandlers.OfType<ObservableServiceHandler>().Single().MessageStream;
+            var serverMessageStream = _serverFactory.InheritedHandlers.OfType<ObservableServiceHandler>().Single().MessageStream;
 
-            var peerRepo = Substitute.For<IRepository<Peer>>();
-            peerRepo.GetAll().Returns(Enumerable.Repeat(new Peer(), 12));
-            var peerCountObserver = new PeerCountRequestObserver(
-                new PeerIdentifier(serverId), peerRepo, Substitute.For<ILogger>());
+            //We can do that and have a HandleRequest mocked
+            //var serverObserver = new TestMessageObserver<GetPeerCountRequest>(Substitute.For<ILogger>());
+            //_serverSubscription = messageStream.Subscribe(serverObserver))
 
-            peerCountObserver.StartObserving(messageStream);
-            using (messageStream.Subscribe(serverObserver))
-            {
-                serverChannel.WriteInbound(sent);
-                await messageStream.WaitForItemsOnDelayedStreamOnTaskPoolScheduler();
+            //but maybe we want to take a real one and let its implementation do the posting back on the server outbound channel
+            _peerRepository = Substitute.For<IRepository<Peer>>();
+            var expectedPeerCount = 12; 
+            _peerRepository.GetAll().Returns(Enumerable.Repeat(new Peer(), expectedPeerCount));
+            _peerCountObserver = new PeerCountRequestObserver(
+                new PeerIdentifier(serverId), _peerRepository, Substitute.For<ILogger>());
 
-                serverObserver.Received.Count.Should().Be(1);
-                serverObserver.Received.Single().Payload.CorrelationId.ToGuid().Should().Be(guid);
+            _peerCountObserver.StartObserving(serverMessageStream);
+            
+            serverChannel.WriteInbound(sent);
+            //await serverMessageStream.WaitForItemsOnDelayedStreamOnTaskPoolScheduler();
 
-                //peerCountObserver.ChannelHandlerContext.Received()
-                //finish that tomorrow  
-            }
+            //serverObserver.SubstituteObserver.Received(1).OnNext(Arg.Any<GetPeerCountRequest>());
+            //serverObserver.HandleRequest(Arg.Any<IProtocolMessageDto<ProtocolMessage>>()).Received(1);
+            //serverObserver.ChannelHandlerContext.Received(1).WriteAndFlushAsync(Arg.Any<IMessage>());
+            //var rawResponse = serverObserver.ChannelHandlerContext.ReceivedCalls().Single().GetArguments().Single();
+
+            //send the response back on the outbound channel
+            //serverChannel.WriteOutbound(rawResponse);
+            //_serverKeySigner.Received(1).Sign(Arg.Any<byte[]>());
+            var response = serverChannel.ReadOutbound<ProtocolMessageSigned>();
+            //response.Should().NotBeNull();
+            //response.Signature.Should().NotBeEmpty();
+
+            var clientMessageStream = _clientFactory.InheritedHandlers.OfType<ObservableServiceHandler>().Single().MessageStream;
+            _clientObserver = new TestMessageObserver<GetPeerCountResponse>(Substitute.For<ILogger>());
+            _clientObserver.StartObserving(clientMessageStream);
+
+            //clientChannel.WriteInbound(response);
+            //await clientMessageStream.WaitForItemsOnDelayedStreamOnTaskPoolScheduler();
+
+            //_clientKeySigner.Received(1).Verify(Arg.Any<IPublicKey>(), Arg.Any<byte[]>(), Arg.Any<ISignature>());
+            //_clientObserver.SubstituteObserver.Received(1).OnNext(Arg.Is<GetPeerCountResponse>(c => c.PeerCount == expectedPeerCount));
+
+            await serverChannel.DisconnectAsync();
+            await clientChannel.DisconnectAsync();
+        }
+
+        public void Dispose()
+        {
+            _peerRepository.Dispose();
+            _peerCountObserver.Dispose();
+            
+            //_serverSubscription.Dispose();
+            _clientObserver.Dispose();
         }
     }
 }
