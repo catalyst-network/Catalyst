67bdb118755a25ca444dc4381fc6fdc317ca71ef
Sanjay Patel <sanjay.patel@atlascity.io>
2019-06-25 16:34:46 +0100

    Remove un-used using

--- src/Catalyst.Node.Core.UnitTests/P2P/Observables/GetNeighbourRequestObserverTests.cs
+++ src/Catalyst.Node.Core.UnitTests/P2P/Observables/GetNeighbourRequestObserverTests.cs
@@ -25,7 +25,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Autofac;
