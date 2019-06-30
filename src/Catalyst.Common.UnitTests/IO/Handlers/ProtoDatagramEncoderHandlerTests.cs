88c1fae710d23c06a6a71e33d99bbf8414991f2e
nshcore <nshcore@protonmail.com>
2019-07-01 00:39:52 +0100

    update

--- src/Catalyst.Common.UnitTests/IO/Handlers/ProtoDatagramEncoderHandlerTests.cs
+++ src/Catalyst.Common.UnitTests/IO/Handlers/ProtoDatagramEncoderHandlerTests.cs
@@ -22,14 +22,24 @@
 #endregion
 
 using System;
+using System.Net;
+using Catalyst.Common.Extensions;
+using Catalyst.Common.Interfaces.IO.Messaging.Dto;
 using Catalyst.Common.IO.Handlers;
 using Catalyst.Common.IO.Messaging.Dto;
+using Catalyst.Common.Util;
+using Catalyst.Cryptography.BulletProofs.Wrapper.Interfaces;
 using Catalyst.Protocol.Common;
+using Catalyst.Protocol.IPPN;
 using Catalyst.TestUtils;
 using DotNetty.Buffers;
+using DotNetty.Codecs;
+using DotNetty.Codecs.Protobuf;
+using DotNetty.Transport.Channels;
 using DotNetty.Transport.Channels.Embedded;
 using DotNetty.Transport.Channels.Sockets;
 using FluentAssertions;
+using Google.Protobuf;
 using NSubstitute;
 using Serilog;
 using Xunit;
@@ -41,18 +51,30 @@ namespace Catalyst.Common.UnitTests.IO.Handlers
         [Fact]
         public void Does_Process_IMessageDto_Types()
         {
-            var handler = new DatagramProtobufEncoder(Substitute.For<ILogger>());
+            // var handler = new DatagramProtobufEncoder(Substitute.For<ILogger>());
+            var handler = new DatagramPacketEncoder<IMessage>(
+                new ProtobufEncoder()
+            );
+
             var channel = new EmbeddedChannel(handler);
 
-            var dto = new MessageDto<ProtocolMessageSigned>(
-                new ProtocolMessageSigned(),
-                PeerIdentifierHelper.GetPeerIdentifier("sender"),
-                PeerIdentifierHelper.GetPeerIdentifier("recipient"),
-                Guid.NewGuid());
-
-            channel.WriteOutbound(dto);
+            var protocolMessageSigned = new ProtocolMessageSigned
+            {
+                Message = new PingRequest().ToProtocolMessage(PeerIdentifierHelper.GetPeerIdentifier("sender").PeerId, Guid.NewGuid()),
+                Signature = ByteUtil.GenerateRandomByteArray(64).ToByteString()
+            };
+           
+            var datagram = new DatagramPacket(Unpooled.WrappedBuffer(protocolMessageSigned.Message.ToByteArray()), new IPEndPoint(IPAddress.Loopback, IPEndPoint.MinPort));
+                
+            channel.WriteOutbound(datagram);
             var packet = channel.ReadOutbound<DatagramPacket>();
             packet.Content.Should().BeAssignableTo<IByteBuffer>();
+            
+            var decoder = new DatagramPacketDecoder(new ProtobufDecoder(ProtocolMessageSigned.Parser));
+            var decoderChannel = new EmbeddedChannel(decoder);
+            decoderChannel.WriteInbound(new ProtocolMessageSigned());
+            var message = decoderChannel.ReadInbound<IMessage>();
+            message.Should().BeAssignableTo<IMessage>();
         }
     }
 }
