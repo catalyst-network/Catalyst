3f2f6e9cb7a4dd0eef89b05bbd4c244a0ec9ef9a
millymanz <dennis565@yahoo.co.uk>
2019-07-04 17:08:53 +0100

    Implemented test for CLI to Node test

--- src/Catalyst.Cli.IntegrationTests/Commands/CliCommandTestBase.cs
+++ src/Catalyst.Cli.IntegrationTests/Commands/CliCommandTestBase.cs
@@ -39,7 +39,7 @@ namespace Catalyst.Cli.IntegrationTests.Commands
 {
     public abstract class CliCommandTestBase : ConfigFileBasedTest
     {
-        protected readonly INodeRpcClient NodeRpcClient;
+        protected INodeRpcClient NodeRpcClient;
 
         public static IEnumerable<object[]> AddFileData =>
             new List<object[]>
@@ -54,7 +54,7 @@ namespace Catalyst.Cli.IntegrationTests.Commands
                 new object[] {"/fake_file_hash", AppDomain.CurrentDomain.BaseDirectory + "/Config/addfile_test.json", true}
             };
 
-        protected CliCommandTestBase(ITestOutputHelper output) : base(output)
+        protected CliCommandTestBase(ITestOutputHelper output, bool bSubstituteNodeClient = true) : base(output)
         {
             var config = new ConfigurationBuilder()
                .AddJsonFile(Path.Combine(Constants.ConfigSubFolder, Constants.ShellComponentsJsonConfigFile))
@@ -63,6 +63,13 @@ namespace Catalyst.Cli.IntegrationTests.Commands
                .AddJsonFile(Path.Combine(Constants.ConfigSubFolder, Constants.ShellConfigFile))
                .Build();
 
+            ConfigureContainerBuilder(config);
+
+            if (bSubstituteNodeClient) ConfigureNodeClient();
+        }
+
+        protected void ConfigureNodeClient()
+        {
             var channel = Substitute.For<IChannel>();
             channel.Active.Returns(true);
 
@@ -74,8 +81,6 @@ namespace Catalyst.Cli.IntegrationTests.Commands
             nodeRpcClientFactory
                .GetClient(Arg.Any<X509Certificate2>(), Arg.Is<IRpcNodeConfig>(c => c.NodeId == "node1"))
                .Returns(NodeRpcClient);
-
-            ConfigureContainerBuilder(config);
 
             ContainerBuilder.RegisterInstance(nodeRpcClientFactory).As<INodeRpcClientFactory>();
         }
---
+++ src/Catalyst.Cli.IntegrationTests/Connection/CliToNodeTest.cs
@@ -0,0 +1,165 @@
+#region LICENSE
+
+/**
+* Copyright (c) 2019 Catalyst Network
+*
+* This file is part of Catalyst.Node <https://github.com/catalyst-network/Catalyst.Node>
+*
+* Catalyst.Node is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Catalyst.Node is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Catalyst.Node. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using Autofac;
+using Catalyst.Common.Interfaces.Cli;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+using System;
+using Catalyst.Common.Interfaces.Rpc;
+using Catalyst.Common.Interfaces.Cryptography;
+using Microsoft.Extensions.Configuration;
+using Catalyst.Node.Core.Modules.Dfs;
+using Catalyst.Common.Config;
+using Catalyst.Common.Enumerator;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using Catalyst.Common.Interfaces;
+using System.Threading;
+using NSubstitute;
+using Serilog;
+using Catalyst.TestUtils;
+using Ipfs.CoreApi;
+using Catalyst.Common.Interfaces.P2P;
+using Catalyst.Cli.IntegrationTests.Commands;
+using Constants = Catalyst.Common.Config.Constants;
+
+
+namespace Catalyst.Cli.IntegrationTests.Connection
+{
+    public sealed class CliToNodeTest : CliCommandTestBase
+    {
+        private NodeTest _node;
+
+        public static readonly List<object[]> Networks = 
+            Enumeration.GetAll<Network>().Select(n => new object[] { n }).ToList();
+
+        public CliToNodeTest(ITestOutputHelper output) : base(output, false)
+        {
+            _node = new NodeTest(output);
+        }
+
+        private class NodeTest : CliCommandTestBase
+        {
+            private CancellationTokenSource _cancellationSource;
+
+            public NodeTest(ITestOutputHelper output) : base(output) { }
+
+            private Network GetNetworkType(string networkType)
+            {
+                Network netTemp = null;
+
+                switch (networkType)
+                {
+                    case "Test_Mode": { netTemp = Network.Test;  } break;
+                    case "Dev_Mode": { netTemp = Network.Dev; } break;
+                    case "Main_Mode": { netTemp = Network.Main; } break;
+                }
+                return netTemp;
+            }
+
+            private void NodeSetup(object network)
+            {
+                var currentNetwork = GetNetworkType(network as string);
+
+                var configFiles = new[]
+                    {
+                    Constants.NetworkConfigFile(currentNetwork),
+                    Constants.ComponentsJsonConfigFile,
+                    Constants.SerilogJsonConfigFile
+                }
+                .Select(f => Path.Combine(Constants.ConfigSubFolder, f));
+
+                var configBuilder = new ConfigurationBuilder();
+                configFiles.ToList().ForEach(f => configBuilder.AddJsonFile(f));
+
+                var configRoot = configBuilder.Build();
+                ConfigureContainerBuilder(configRoot);
+            }
+
+            public void StartNode(string networkType)
+            {
+                var threadStart = new ParameterizedThreadStart(RunNodeInstance);
+                var thread = new Thread(threadStart);
+                thread.Start(networkType);
+            }
+
+            private IpfsAdapter ConfigureKeyTestDependency()
+            {
+                var peerSettings = Substitute.For<IPeerSettings>();
+                peerSettings.SeedServers.Returns(new[]
+                {
+                "seed1.server.va",
+                "island.domain.tv"
+            });
+
+                var passwordReader = Substitute.For<IPasswordReader>();
+                passwordReader.ReadSecurePassword().ReturnsForAnyArgs(TestPasswordReader.BuildSecureStringPassword("trendy"));
+                var logger = Substitute.For<ILogger>();
+                return new IpfsAdapter(passwordReader, peerSettings, FileSystem, logger);
+            }
+
+            private void RunNodeInstance(object network)
+            {
+                NodeSetup(network);
+
+                _cancellationSource = new CancellationTokenSource();
+
+                var ipfs = ConfigureKeyTestDependency();
+                ContainerBuilder.RegisterInstance(ipfs).As<ICoreApi>();
+
+                using (var container = ContainerBuilder.Build())
+                {
+                    using (var scope = container.BeginLifetimeScope("testnet"))
+                    {
+                        var node = scope.Resolve<ICatalystNode>();
+                        node.RunAsync(_cancellationSource.Token).Wait(_cancellationSource.Token);
+                    }
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData("Test_Mode")]
+        [InlineData("Dev_Mode")]
+        [InlineData("Main_Mode")]
+        public void CliToNode_Connect_To_Node(string netType)
+        {
+            _node.StartNode(netType);
+
+            Thread.Sleep(4000);
+
+            using (var container = ContainerBuilder.Build())
+            {
+                using (container.BeginLifetimeScope(CurrentTestName))
+                {
+                    var shell = container.Resolve<ICatalystCli>();
+                    var hasConnected = shell.AdvancedShell.ParseCommand("connect", "-n", "node1");
+                    hasConnected.Should().BeTrue();
+                }
+            }
+        }
+    }
+}
