b0e1ae832a240c0aa6546a64d41d60fa470f5ee2
millymanz <dennis565@yahoo.co.uk>
2019-07-08 17:24:46 +0100

    place holder code

--- src/Catalyst.Node.Core.IntegrationTests/P2P/PeerValidationIntegrationTest.cs
+++ src/Catalyst.Node.Core.IntegrationTests/P2P/PeerValidationIntegrationTest.cs
@@ -21,9 +21,11 @@
 
 #endregion
 
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Reactive.Linq;
 using Autofac;
 using Catalyst.Common.Config;
@@ -33,6 +35,7 @@ using Catalyst.Common.Interfaces.P2P;
 using Catalyst.Common.IO.Messaging.Correlation;
 using Catalyst.Common.IO.Messaging.Dto;
 using Catalyst.Common.Network;
+using Catalyst.Common.P2P;
 using Catalyst.Node.Core.P2P;
 using Catalyst.Protocol.IPPN;
 using Catalyst.TestUtils;
@@ -54,37 +57,27 @@ namespace Catalyst.Node.Core.IntegrationTests.P2P
 {
     public sealed class PeerValidationIntegrationTest : ConfigFileBasedTest
     {
-        private readonly IConfigurationRoot _config;
-        private readonly IDns _dns;
-        private readonly IRepository<Peer> _peerRepository;
+        private readonly Guid _guid;
         private readonly ILogger _logger;
-        private readonly ILookupClient _lookupClient;
-        private readonly List<string> _dnsDomains;
-        private readonly string _seedPid;
-
+        private readonly IPeerIdentifier _pid;
+        private readonly IContainer _container;
+        private readonly PingRequest _pingRequest;
+        private readonly IConfigurationRoot _config;
         public PeerValidationIntegrationTest(ITestOutputHelper output) : base(output)
         {
-            _peerRepository = Substitute.For<IRepository<Peer>>();
-            _logger = Substitute.For<ILogger>();
-            _lookupClient = Substitute.For<ILookupClient>();
-            _dns = new Common.Network.DnsClient(_lookupClient, Substitute.For<IPeerIdValidator>());
-
-            _config = new ConfigurationBuilder()
+            _config = SocketPortHelper.AlterConfigurationToGetUniquePort(new ConfigurationBuilder()
                .AddJsonFile(Path.Combine(Constants.ConfigSubFolder, Constants.ComponentsJsonConfigFile))
                .AddJsonFile(Path.Combine(Constants.ConfigSubFolder, Constants.SerilogJsonConfigFile))
-               .AddJsonFile(Path.Combine(Constants.ConfigSubFolder, Constants.NetworkConfigFile(Network.Dev)))
-               .Build();
-            
-            _dnsDomains = new List<string>
-            {
-                "seed1.catalystnetwork.io",
-                "seed2.catalystnetwork.io",
-                "seed3.catalystnetwork.io",
-                "seed4.catalystnetwork.io",
-                "seed5.catalystnetwork.io"
-            };
-            
-            _seedPid = "0x41437c30317c39322e3230372e3137382e3139387c34323036397c3031323334353637383930313233343536373839323232323232323232323232";
+               .AddJsonFile(Path.Combine(Constants.ConfigSubFolder, Constants.NetworkConfigFile(Network.Test)))
+               .Build(), CurrentTestName);
+            _pid = PeerIdentifierHelper.GetPeerIdentifier("im_a_key");
+            _guid = Guid.NewGuid();
+            _logger = Substitute.For<ILogger>();
+            _pingRequest = new PingRequest();
+
+            ConfigureContainerBuilder(_config, true, true);
+
+            _container = ContainerBuilder.Build();
         }
 
         [Fact]
@@ -109,60 +102,39 @@ namespace Catalyst.Node.Core.IntegrationTests.P2P
             }
         }
 
-        [Fact]
-        public void CanParseDnsNodesFromConfig()
+        [Theory(Skip = "due to peer service refactor")]
+        [Trait(Traits.TestType, Traits.IntegrationTest)]
+        [InlineData("Fr2a300k06032b657793", "92.207.178.198", 1574)]
+        [InlineData("pp2a300k55032b657791", "198.51.100.3", 2524)]
+        public void PeerChallenge_PeerIdentifiers_Expect_To_Fail_IP_Port_PublicKey(string publicKey, string ip, int port)
         {
-            _dnsDomains.ForEach(domain =>
+            using (_container.BeginLifetimeScope(CurrentTestName))
             {
-                MockQueryResponse.CreateFakeLookupResult(domain, _seedPid, _lookupClient);
-            });
-            
-            var peerDiscovery = new PeerDiscovery(_dns, _peerRepository, _config, _logger);
-
-            var seedServers = peerDiscovery.ParseDnsServersFromConfig(_config);
-
-            seedServers.Should().NotBeNullOrEmpty();
-            seedServers.Should().Contain(_dnsDomains);
+            }
         }
 
-        [Fact]
-        public void CanGetSeedNodesFromDns()
-        {
-            _dnsDomains.ForEach(domain =>
-            {
-                MockQueryResponse.CreateFakeLookupResult(domain, _seedPid, _lookupClient);
-            });
-
-            var peerDiscovery = new PeerDiscovery(_dns, _peerRepository, _config, _logger);
-
-            peerDiscovery.Peers.Should().NotBeNullOrEmpty();
-            peerDiscovery.Peers.Should().HaveCount(1);
-            peerDiscovery.Peers.Should().NotContainNulls();
-            peerDiscovery.Peers.Should().ContainItemsAssignableTo<IPeerIdentifier>();
-        }
-
-        [Fact]
-        public void CanReceivePingEventsFromSubscribedStream()
-        {
-            _dnsDomains.ForEach(domain =>
-            {
-                MockQueryResponse.CreateFakeLookupResult(domain, _seedPid, _lookupClient);
-            });
+        //[Fact]
+        //public void CanReceivePingEventsFromSubscribedStream()
+        //{
+        //    _dnsDomains.ForEach(domain =>
+        //    {
+        //        MockQueryResponse.CreateFakeLookupResult(domain, _seedPid, _lookupClient);
+        //    });
             
-            var peerDiscovery = new PeerDiscovery(_dns, _peerRepository, _config, _logger);
+        //    var peerDiscovery = new PeerDiscovery(_dns, _peerRepository, _config, _logger);
 
-            var fakeContext = Substitute.For<IChannelHandlerContext>();
-            var pingRequest = new PingResponse();
-            var pid = PeerIdentifierHelper.GetPeerIdentifier("im_a_key");
-            var channeledAny = new ObserverDto(fakeContext, 
-                pingRequest.ToProtocolMessage(pid.PeerId, CorrelationId.GenerateCorrelationId()));
+        //    var fakeContext = Substitute.For<IChannelHandlerContext>();
+        //    var pingRequest = new PingResponse();
+        //    var pid = PeerIdentifierHelper.GetPeerIdentifier("im_a_key");
+        //    var channeledAny = new ObserverDto(fakeContext, 
+        //        pingRequest.ToProtocolMessage(pid.PeerId, CorrelationId.GenerateCorrelationId()));
             
-            var observableStream = new[] {channeledAny}.ToObservable();
+        //    var observableStream = new[] {channeledAny}.ToObservable();
 
-            peerDiscovery.StartObserving(observableStream);
+        //    peerDiscovery.StartObserving(observableStream);
 
-            _peerRepository.Received(1)
-               .Add(Arg.Is<Peer>(p => p.PeerIdentifier.Equals(pid)));
-        }
+        //    _peerRepository.Received(1)
+        //       .Add(Arg.Is<Peer>(p => p.PeerIdentifier.Equals(pid)));
+        //}
     }
 }
