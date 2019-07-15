b7307e6916f3dca0c6d63d4999e77569dd49670c
monsieurleberre <monsieur.leberre@gmail.com>
2019-07-15 15:51:35 +0100

    fix merge

--- src/Catalyst.Cli.IntegrationTests/Commands/GetPeerInfoCommandTests.cs
+++ src/Catalyst.Cli.IntegrationTests/Commands/GetPeerInfoCommandTests.cs
@@ -32,8 +32,6 @@ namespace Catalyst.Cli.IntegrationTests.Commands
 {
     public sealed class GetPeerInfoCommandTests : CliCommandTestBase
     {
-        //This test is the base to all other tests.  If the Cli cannot connect to a node than all other commands
-        //will fail
         public GetPeerInfoCommandTests(ITestOutputHelper output) : base(output) { }
 
         [Fact]
@@ -44,7 +42,7 @@ namespace Catalyst.Cli.IntegrationTests.Commands
             
             var result = Shell.ParseCommand("getpeerinfo", NodeArgumentPrefix, ServerNodeName, "-i", ipAddress, "-k", publicKey);
             result.Should().BeTrue();
-            AssertSentMessage<GetPeerInfoRequest>();
+            AssertSentMessageAndGetMessageContent<GetPeerInfoRequest>();
         }
     }
 }
