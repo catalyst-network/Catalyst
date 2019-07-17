b586c5cd36c4b4b0a9750cd4736340278e09c49d
nshcore <nshcore@protonmail.com>
2019-07-17 16:10:58 +0100

    HastingMemento
    Implementation memento pattern for hastings discovery

---
+++ src/Catalyst.Core.Lib/Modules/Dfs/IpfsAdapter.cs
@@ -0,0 +1,175 @@
+#region LICENSE
+
+/**
+* Copyright (c) 2019 Catalyst Network
+*
+* This file is part of Catalyst.Node <https://github.com/catalyst-network/Catalyst.Node>
+*
+* Catalyst.Node is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Catalyst.Node is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Catalyst.Node. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using System.IO;
+using System.Linq;
+using Catalyst.Common.Config;
+using Catalyst.Common.Interfaces.Cryptography;
+using Catalyst.Common.Interfaces.FileSystem;
+using Catalyst.Common.Interfaces.P2P;
+using Common.Logging.Serilog;
+using Dawn;
+using Ipfs;
+using Ipfs.CoreApi;
+using Ipfs.Engine;
+using Serilog;
+
+namespace Catalyst.Core.Lib.Modules.Dfs
+{
+    /// <summary>
+    ///   Modifies the IPFS behaviour to meet the Catalyst requirements.
+    /// </summary>
+    /// <remarks>
+    ///   The IPFS engine is lazy, it is only started when needed.
+    /// </remarks>
+    public sealed class IpfsAdapter : ICoreApi, IDisposable
+    {
+        /// <summary>
+        ///   An IPFS implementation, commonly called an IPFS node/
+        /// </summary>
+        private IpfsEngine _ipfs;
+
+        private bool _isStarted;
+        private readonly object _startingLock = new object();
+        private readonly ILogger _logger;
+
+        static IpfsAdapter()
+        {
+            global::Common.Logging.LogManager.Adapter = new SerilogFactoryAdapter(Log.Logger);
+        }
+
+        public IpfsAdapter(IPasswordReader passwordReader, 
+            IPeerSettings peerSettings, 
+            IFileSystem fileSystem, 
+            ILogger logger)
+        {
+            Guard.Argument(peerSettings, nameof(peerSettings)).NotNull()
+               .Require(p => p.SeedServers != null && p.SeedServers.Count > 0,
+                    p => $"{nameof(peerSettings)} needs to specify at least one seed server.");
+
+            _logger = logger;
+
+            // The passphrase is used to access the private keys.
+            var passphrase = passwordReader.ReadSecurePassword("Please provide your IPFS password");
+            _ipfs = new IpfsEngine(passphrase);
+            _ipfs.Options.KeyChain.DefaultKeyType = Constants.KeyChainDefaultKeyType;
+
+            // The IPFS repository is inside the catalyst home folder.
+            _ipfs.Options.Repository.Folder = Path.Combine(
+                fileSystem.GetCatalystDataDir().FullName,
+                Constants.DfsDataSubDir);
+
+            // The seed nodes for the catalyst network.
+            _ipfs.Options.Discovery.BootstrapPeers = peerSettings
+               .SeedServers
+               .Select(s => $"/dns/{s}/tcp/4001")
+               .Select(ma => new MultiAddress(ma))
+               .ToArray();
+
+            // Do not use the public IPFS network, use a private network
+            // of catalyst only nodes.
+            _ipfs.Options.Swarm.PrivateNetworkKey = new PeerTalk.Cryptography.PreSharedKey
+            {
+                Value = Constants.SwarmKey.ToHexBuffer()
+            };
+
+            _logger.Information("IPFS configured.");
+        }
+
+        /// <summary>
+        ///   Starts the engine if required.
+        /// </summary>
+        /// <returns>
+        ///   The started IPFS Engine.
+        /// </returns>
+        private IpfsEngine Start()
+        {
+            if (!_isStarted)
+            {
+                lock (_startingLock)
+                {
+                    if (!_isStarted)
+                    {
+                        _ipfs.Start();
+                        _isStarted = true;
+                        _logger.Information("IPFS started.");
+                    }
+                }
+            }
+
+            return _ipfs;
+        }
+
+        public IBitswapApi Bitswap => Start().Bitswap;
+
+        public IBlockApi Block => Start().Block;
+
+        public IBlockRepositoryApi BlockRepository => Start().BlockRepository;
+
+        public IBootstrapApi Bootstrap => Start().Bootstrap;
+
+        public IConfigApi Config => _ipfs.Config;
+        public IpfsEngineOptions Options => _ipfs.Options;
+
+        public IDagApi Dag => Start().Dag;
+
+        public IDhtApi Dht => Start().Dht;
+
+        public IDnsApi Dns => Start().Dns;
+
+        public IFileSystemApi FileSystem => Start().FileSystem;
+
+        public IGenericApi Generic => Start().Generic;
+
+        public IKeyApi Key => _ipfs.Key;
+
+        public INameApi Name => Start().Name;
+
+        public IObjectApi Object => Start().Object;
+
+        public IPinApi Pin => Start().Pin;
+
+        public IPubSubApi PubSub => Start().PubSub;
+
+        public IStatsApi Stats => Start().Stats;
+
+        public ISwarmApi Swarm => Start().Swarm;
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposing)
+            {
+                return;
+            }
+
+            _ipfs?.Dispose();
+            _ipfs = null;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
---
+++ src/Catalyst.Node.Rpc.Client.UnitTests/IO/Observers/GetDeltaResponseObserverTests.cs
@@ -0,0 +1,105 @@
+#region LICENSE
+
+/**
+* Copyright (c) 2019 Catalyst Network
+*
+* This file is part of Catalyst.Node <https://github.com/catalyst-network/Catalyst.Node>
+*
+* Catalyst.Node is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Catalyst.Node is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Catalyst.Node. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalyst.Common.Extensions;
+using Catalyst.Common.Interfaces.Cli;
+using Catalyst.Common.Interfaces.IO.Messaging.Dto;
+using Catalyst.Common.IO.Messaging.Correlation;
+using Catalyst.Node.Rpc.Client.IO.Observers;
+using Catalyst.Protocol.Common;
+using Catalyst.Protocol.Rpc.Node;
+using Catalyst.TestUtils;
+using DotNetty.Transport.Channels;
+using FluentAssertions;
+using Multiformats.Hash;
+using NSubstitute;
+using Serilog;
+using Xunit;
+
+namespace Catalyst.Node.Rpc.Client.UnitTests.IO.Observers
+{
+    public class GetDeltaResponseObserverTests
+    {
+        private readonly IUserOutput _output;
+        private readonly GetDeltaResponseObserver _observer;
+        private readonly Multihash _previousDeltaHash;
+
+        public GetDeltaResponseObserverTests()
+        {
+            _output = Substitute.For<IUserOutput>();
+            var logger = Substitute.For<ILogger>();
+            _observer = new GetDeltaResponseObserver(_output, logger);
+            var hashingAlgorithm = Common.Config.Constants.HashAlgorithm;
+            _previousDeltaHash = Multihash.Cast(hashingAlgorithm.ComputeHash(Encoding.UTF8.GetBytes("previous")));
+        }
+
+        [Fact]
+        public async Task GetDeltaResponseObserver_Can_Output_Delta_As_Json()
+        {
+            var deltaContent = DeltaHelper.GetDelta(_previousDeltaHash);
+            var deltaResponse = new GetDeltaResponse {Delta = deltaContent};
+
+            var messageStream = CreateMessageStream(deltaResponse);
+
+            _observer.StartObserving(messageStream);
+
+            await messageStream.WaitForEndOfDelayedStreamOnTaskPoolSchedulerAsync();
+
+            _output.Received(1).WriteLine(Arg.Any<string>());
+
+            var messageOutput = (string) _output.ReceivedCalls().Single().GetArguments().Single();
+            messageOutput.Should().Be(deltaContent.ToString());
+        }
+
+        [Fact]
+        public async Task GetDeltaResponseObserver_Treats_Null_Content_As_Not_Found()
+        {
+            var deltaResponse = new GetDeltaResponse {Delta = null};
+
+            var messageStream = CreateMessageStream(deltaResponse);
+
+            _observer.StartObserving(messageStream);
+
+            await messageStream.WaitForEndOfDelayedStreamOnTaskPoolSchedulerAsync();
+
+            _output.Received(1).WriteLine(Arg.Any<string>());
+
+            var messageOutput = (string) _output.ReceivedCalls().Single().GetArguments().Single();
+            messageOutput.Should().Be(GetDeltaResponseObserver.UnableToRetrieveDeltaMessage);
+        }
+
+        private static IObservable<IObserverDto<ProtocolMessage>> CreateMessageStream(GetDeltaResponse deltaResponse)
+        {
+            var messageStream = MessageStreamHelper.CreateStreamWithMessage(Substitute.For<IChannelHandlerContext>(),
+                deltaResponse.ToProtocolMessage(
+                    PeerIdHelper.GetPeerId("sender"),
+                    CorrelationId.GenerateCorrelationId()));
+            return messageStream;
+        }
+    }
+}
+
