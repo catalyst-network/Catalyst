10a3704e9042642c39105b3626ae9fe168d223a5
nshcore <nshcore@protonmail.com>
2019-07-17 18:04:46 +0100

    wip

--- src/Catalyst.Core.Lib.IntegrationTests/P2P/Discovery/HastingDiscoveryTests.cs
+++ src/Catalyst.Core.Lib.IntegrationTests/P2P/Discovery/HastingDiscoveryTests.cs
@@ -29,11 +29,15 @@ using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Catalyst.Common.Config;
+using Catalyst.Common.Extensions;
+using Catalyst.Common.Interfaces.IO.Messaging.Dto;
 using Catalyst.Common.Interfaces.P2P;
 using Catalyst.Common.Interfaces.P2P.Discovery;
 using Catalyst.Common.Interfaces.P2P.IO;
 using Catalyst.Common.Interfaces.P2P.IO.Messaging.Dto;
 using Catalyst.Common.Interfaces.P2P.ReputationSystem;
+using Catalyst.Common.Interfaces.Util;
+using Catalyst.Common.IO.Messaging.Correlation;
 using Catalyst.Common.IO.Messaging.Dto;
 using Catalyst.Common.P2P;
 using Catalyst.Common.Util;
@@ -42,47 +46,191 @@ using Catalyst.Core.Lib.P2P.Discovery;
 using Catalyst.Core.Lib.P2P.IO.Messaging.Correlation;
 using Catalyst.Core.Lib.P2P.IO.Messaging.Dto;
 using Catalyst.Core.Lib.P2P.IO.Observers;
+using Catalyst.Protocol.Common;
 using Catalyst.Protocol.IPPN;
 using Catalyst.TestUtils;
+using FluentAssertions;
+using Google.Protobuf;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
 using NSubstitute;
 using Serilog;
 using SharpRepository.InMemoryRepository;
+using SharpRepository.Repository;
+using SharpRepository.Repository.Caching;
 using Xunit;
 
 namespace Catalyst.Core.Lib.IntegrationTests.P2P.Discovery
 {
     public sealed class HastingDiscoveryTests
     {
-        private List<string> _dnsDomains;
-        private PeerSettings _settings;
+        private IPeerSettings _settings;
         private ILogger _logger;
         private IPeerIdentifier _ownNode;
 
         public HastingDiscoveryTests()
         {
-            _dnsDomains = new List<string>
-            {
-                "seed1.catalystnetwork.io",
-                "seed2.catalystnetwork.io",
-                "seed3.catalystnetwork.io",
-                "seed4.catalystnetwork.io",
-                "seed5.catalystnetwork.io"
-            };
-            
-            _settings = new PeerSettings(new ConfigurationBuilder()
-               .AddJsonFile(Path.Combine(Constants.ConfigSubFolder, Constants.ComponentsJsonConfigFile))
-               .AddJsonFile(Path.Combine(Constants.ConfigSubFolder, Constants.SerilogJsonConfigFile))
-               .AddJsonFile(Path.Combine(Constants.ConfigSubFolder, Constants.NetworkConfigFile(Network.Dev)))
-               .Build());
-            
+            _settings = PeerSettingsHelper.TestPeerSettings();
             _logger = Substitute.For<ILogger>();
             _ownNode = PeerIdentifierHelper.GetPeerIdentifier("ownNode");
         }
+        
+        /// <summary>
+        ///  clean up
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="cache"></param>
+        private void AddCreateEntryExpectation(object key, IMemoryCache cache)
+        {
+            var correlationId = (ByteString) key;
+            var cacheEntry = Substitute.For<ICacheEntry>();
+            var expirationTokens = new List<IChangeToken>();
+            cacheEntry.ExpirationTokens.Returns(expirationTokens);
+            var expirationCallbacks = new List<PostEvictionCallbackRegistration>();
+            cacheEntry.PostEvictionCallbacks.Returns(expirationCallbacks);
+
+            cache.CreateEntry(correlationId).Returns(cacheEntry);
+            CacheEntriesByRequest.Add(correlationId, cacheEntry);
+        }
+
+        protected readonly Dictionary<ByteString, ICacheEntry> CacheEntriesByRequest 
+            = new Dictionary<ByteString, ICacheEntry>();
 
         [Fact]
-        public async Task Evicted_Known_Ping_Message_Sets_Contacted_Neighbour_As_UnReachable() { }
+        public async Task Evicted_Known_Ping_Message_Sets_Contacted_Neighbour_As_UnReachable()
+        {
+            var pr = new PingResponseObserver(Substitute.For<ILogger>());
+            var peerClientObservers = new List<IPeerClientObservable> {pr};
+
+            var seedState = HastingDiscoveryHelper.GenerateSeedState(_ownNode, _settings.SeedServers.ToList(), _settings);
+            var seedOrigin = new HastingsOriginator();
+            seedOrigin.SetMemento(seedState);
+            
+            var stateCareTaker = new HastingCareTaker();
+            var stateHistory = new Stack<IHastingMemento>();
+            stateHistory.Push(seedState);
+            
+            HastingDiscoveryHelper.GenerateMementoHistory(stateHistory, 5).ToList().ForEach(i => stateCareTaker.Add(i));
+            
+            var knownPnr = HastingDiscoveryHelper.MockPnr();
+            var stateCandidate = HastingDiscoveryHelper.MockOriginator();
+            stateCandidate.ExpectedPnr = knownPnr;
+            stateCandidate.CurrentPeersNeighbours.Clear();
+
+            var memoryCache = Substitute.For<IMemoryCache>();
+            
+            var peerMessageCorrelationManager = new PeerMessageCorrelationManager(
+                Substitute.For<IReputationManager>(),
+                memoryCache,
+                Substitute.For<ILogger>(),
+                new TtlChangeTokenProvider(3));
+                
+            stateCandidate.ContactedNeighbours.ToList().ForEach(i =>
+            {
+                AddCreateEntryExpectation(i.Key.Id.ToByteString(), memoryCache);
+
+                peerMessageCorrelationManager.AddPendingRequest(
+                    new CorrelatableMessage<ProtocolMessage>
+                    {
+                        Content = new PingRequest().ToProtocolMessage(_ownNode.PeerId, i.Key),
+                        Recipient = i.Value
+                    });
+
+                var x = CacheEntriesByRequest[i.Key.Id.ToByteString()].PostEvictionCallbacks.Count;
+            });
+            
+            using (var walker = new HastingsDiscovery(
+                Substitute.For<ILogger>(),
+                Substitute.For<IRepository<Peer>>(),
+                HastingDiscoveryHelper.SubDnsClient(_settings.SeedServers.ToList(), _settings),
+                _settings,
+                Substitute.For<IPeerClient>(),
+                Substitute.For<IDtoFactory>(),
+                peerMessageCorrelationManager,
+                Substitute.For<ICancellationTokenProvider>(),
+                peerClientObservers,
+                false,
+                0,
+                seedOrigin,
+                stateCareTaker,
+                stateCandidate))
+            {
+                CacheEntriesByRequest.ToList().ForEach(i => i.Value.PostEvictionCallbacks.FirstOrDefault().EvictionCallback.Invoke());
+                CacheEntriesByRequest[stateCandidate.ContactedNeighbours[0].Key.Id.ToByteString()].PostEvictionCallbacks[0].EvictionCallback
+                   .Invoke();
+            }
+        }
+
+        [Fact]
+        public async Task Expected_Ping_Response_From_All_Contacted_Nodes_Produces_Valid_State_Candidate()
+        {
+            var pr = new PingResponseObserver(Substitute.For<ILogger>());
+            var peerClientObservers = new List<IPeerClientObservable> {pr};
+
+            var seedState = HastingDiscoveryHelper.GenerateSeedState(_ownNode, _settings.SeedServers.ToList(), _settings);
+            var seedOrigin = new HastingsOriginator();
+            seedOrigin.SetMemento(seedState);
+            
+            var stateCareTaker = new HastingCareTaker();
+            var stateHistory = new Stack<IHastingMemento>();
+            stateHistory.Push(seedState);
+            
+            HastingDiscoveryHelper.GenerateMementoHistory(stateHistory, 5).ToList().ForEach(i => stateCareTaker.Add(i));
+            
+            var knownPnr = HastingDiscoveryHelper.MockPnr();
+            var stateCandidate = HastingDiscoveryHelper.MockOriginator();
+            stateCandidate.ExpectedPnr = knownPnr;
+            stateCandidate.CurrentPeersNeighbours.Clear();
+
+            using (var walker = new HastingsDiscovery(
+                Substitute.For<ILogger>(),
+                Substitute.For<IRepository<Peer>>(),
+                HastingDiscoveryHelper.SubDnsClient(_settings.SeedServers.ToList(), _settings),
+                _settings,
+                Substitute.For<IPeerClient>(),
+                Substitute.For<IDtoFactory>(),
+                new PeerMessageCorrelationManager(
+                    Substitute.For<IReputationManager>(),
+                    Substitute.For<IMemoryCache>(),
+                    Substitute.For<ILogger>(),
+                    new TtlChangeTokenProvider(3)),
+                Substitute.For<ICancellationTokenProvider>(),
+                peerClientObservers,
+                false,
+                0,
+                seedOrigin,
+                stateCareTaker,
+                stateCandidate))
+            {
+                var streamObserver = Substitute.For<IObserver<IPeerClientMessageDto>>();
+
+                IList<IPeerClientMessageDto> dtoList = new List<IPeerClientMessageDto>();
+                
+                stateCandidate.ContactedNeighbours.ToList().ForEach(i =>
+                {
+                    var dto = new PeerClientMessageDto(new PingResponse(),
+                        stateCandidate.ContactedNeighbours.FirstOrDefault().Value,
+                        stateCandidate.ContactedNeighbours.FirstOrDefault().Key
+                    );
+                    
+                    dtoList.Add(dto);
+                    pr._responseMessageSubject.OnNext(dto);
+                });
+                
+                using (walker.DiscoveryStream.SubscribeOn(TaskPoolScheduler.Default)
+                   .Subscribe(streamObserver.OnNext))
+                {
+                    walker.StateCandidate.CurrentPeersNeighbours
+                       .Select(i => i.PeerId)
+                       .Should()
+                       .BeSubsetOf(
+                            stateCandidate.ContactedNeighbours
+                               .Select(i => i.Value.PeerId)
+                        );
+                }
+            }
+        }
 
         [Fact]
         public async Task Expected_Ping_Response_Sets_Neighbour_As_Reachable()
@@ -90,7 +238,7 @@ namespace Catalyst.Core.Lib.IntegrationTests.P2P.Discovery
             var pr = new PingResponseObserver(Substitute.For<ILogger>());
             var peerClientObservers = new List<IPeerClientObservable> {pr};
 
-            var seedState = HastingDiscoveryHelper.GenerateSeedState(_ownNode, _dnsDomains, _settings);
+            var seedState = HastingDiscoveryHelper.GenerateSeedState(_ownNode, _settings.SeedServers.ToList(), _settings);
             var seedOrigin = new HastingsOriginator();
             seedOrigin.SetMemento(seedState);
 
@@ -107,17 +255,17 @@ namespace Catalyst.Core.Lib.IntegrationTests.P2P.Discovery
 
             using (var walker = new HastingsDiscovery(
                 Substitute.For<ILogger>(),
-                new InMemoryRepository<Peer>(), 
-                HastingDiscoveryHelper.SubDnsClient(_dnsDomains, _settings),
+                Substitute.For<IRepository<Peer>>(), 
+                HastingDiscoveryHelper.SubDnsClient(_settings.SeedServers.ToList(), _settings),
                 _settings,
                 Substitute.For<IPeerClient>(),
-                new DtoFactory(),
+                Substitute.For<IDtoFactory>(),
                 new PeerMessageCorrelationManager(
                     Substitute.For<IReputationManager>(),
                     Substitute.For<IMemoryCache>(),
                     Substitute.For<ILogger>(),
                     new TtlChangeTokenProvider(3)),
-                new CancellationTokenProvider(),
+                Substitute.For<ICancellationTokenProvider>(),
                 peerClientObservers,
                 false,
                 0,
