37133fda8ac821822fb2a9da15cf3fcdcd86e40d
Sanjay Patel <sanjay.patel@atlascity.io>
2019-07-23 15:31:38 +0100

    Remove un-used import

--- src/Catalyst.Core.Lib/P2P/IO/Observers/TransactionBroadcastObserver.cs
+++ src/Catalyst.Core.Lib/P2P/IO/Observers/TransactionBroadcastObserver.cs
@@ -23,7 +23,6 @@
 
 using Catalyst.Common.Interfaces.IO.Messaging.Dto;
 using Catalyst.Common.Interfaces.IO.Observers;
-using Catalyst.Common.Interfaces.P2P.IO.Messaging.Broadcast;
 using Catalyst.Common.IO.Observers;
 using Catalyst.Protocol;
 using Catalyst.Protocol.Common;
