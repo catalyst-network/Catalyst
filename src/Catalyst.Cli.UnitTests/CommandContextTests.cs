a63205cfcc403cc12752ee1be3314aab2c170cc9
Alex White <alex.white@atlascity.io>
2019-07-26 14:21:31 +0100

    Fix unit test

---
+++ src/Catalyst.Cli.UnitTests/CommandContextTests.cs
@@ -0,0 +1,103 @@
+#region LICENSE
+
+/**
+* Copyright (c) 2019 Catalyst Network
+*
+* This file is part of Catalyst.Node <https://github.com/catalyst-network/Catalyst.Node>
+*
+* Catalyst.Node is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Catalyst.Node is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Catalyst.Node. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using Catalyst.Common.Interfaces.Cli;
+using Catalyst.Common.Interfaces.Cryptography;
+using Catalyst.Common.Interfaces.IO.Messaging.Dto;
+using Catalyst.Common.Interfaces.P2P;
+using Catalyst.Common.Interfaces.Rpc;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using NSubstitute;
+using Serilog;
+using Xunit;
+
+namespace Catalyst.Cli.UnitTests
+{
+    public sealed class CommandContextTests
+    {
+        public CommandContextTests()
+        {
+            var configRoot = Substitute.For<IConfigurationRoot>();
+            var logger = Substitute.For<ILogger>();
+            var userOutput = Substitute.For<IUserOutput>();
+            var peerIdClientId = Substitute.For<IPeerIdClientId>();
+            var dtoFactory = Substitute.For<IDtoFactory>();
+            var nodeRpcClientFactory = Substitute.For<INodeRpcClientFactory>();
+            var certificateStore = Substitute.For<ICertificateStore>();
+            _commandContext = new CommandContext(configRoot, logger, userOutput, peerIdClientId, dtoFactory,
+                nodeRpcClientFactory, certificateStore);
+        }
+
+        private readonly ICommandContext _commandContext;
+
+        [Fact]
+        public void GetConnectedNode_Should_Throw_ArgumentException_On_Empty_NodeId()
+        {
+            Assert.Throws<ArgumentException>(() => _commandContext.GetConnectedNode(string.Empty));
+        }
+
+        [Fact]
+        public void GetConnectedNode_Should_Throw_ArgumentNullException_On_Null_NodeId()
+        {
+            Assert.Throws<ArgumentNullException>(() => _commandContext.GetConnectedNode(null));
+        }
+
+        [Fact]
+        public void GetNodeConfig_Should_Return_Null_On_EmptyConfigs()
+        {
+            _commandContext.GetNodeConfig("No_node_config").Should().BeNull();
+        }
+
+        [Fact]
+        public void GetNodeConfig_Should_Throw_ArgumentException_On_Empty_NodeId()
+        {
+            Assert.Throws<ArgumentException>(() => _commandContext.GetNodeConfig(string.Empty));
+        }
+
+        [Fact]
+        public void GetNodeConfig_Should_Throw_ArgumentNullException_On_Null_NodeId()
+        {
+            Assert.Throws<ArgumentNullException>(() => _commandContext.GetNodeConfig(null));
+        }
+
+        [Fact]
+        public void IsSocketChannelActive_Should_Return_False_On_Inactive_Channel()
+        {
+            var rpcClient = Substitute.For<INodeRpcClient>();
+            rpcClient.Channel.Active.Returns(false);
+
+            _commandContext.IsSocketChannelActive(rpcClient);
+        }
+
+        [Fact]
+        public void IsSocketChannelActive_Should_Return_True_On_Active_Channel()
+        {
+            var rpcClient = Substitute.For<INodeRpcClient>();
+            rpcClient.Channel.Active.Returns(true);
+
+            _commandContext.IsSocketChannelActive(rpcClient);
+        }
+    }
+}
