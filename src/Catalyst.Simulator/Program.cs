7d967e66da8a53c34c116830f44b72174034edae
Alex White <alex.white@atlascity.io>
2019-08-13 10:28:50 +0100

    Quick dirty build of a transaction simulator

--- src/Catalyst.Simulator/Program.cs
+++ src/Catalyst.Simulator/Program.cs
@@ -1,42 +1,70 @@
+#region LICENSE
+
+/**
+* Copyright (c) 2019 Catalyst Network
+*
+* This file is part of Catalyst.Node <https://github.com/catalyst-network/Catalyst.Node>
+*
+* Catalyst.Node is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Catalyst.Node is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Catalyst.Node. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
 using System;
-using System.Net;
-using System.Threading;
+using System.Security;
 using System.Threading.Tasks;
-using Catalyst.Common.Cryptography;
-using Catalyst.Common.FileSystem;
-using Catalyst.Common.IO.EventLoop;
-using Catalyst.Common.IO.Messaging.Dto;
-using Catalyst.Common.Keystore;
-using Catalyst.Common.Modules.KeySigner;
-using Catalyst.Common.P2P;
 using Catalyst.Common.Registry;
-using Catalyst.Common.Rpc.IO.Messaging.Correlation;
-using Catalyst.Common.Shell;
-using Catalyst.Common.Util;
-using Catalyst.Cryptography.BulletProofs.Wrapper;
-using Catalyst.Node.Rpc.Client;
-using Catalyst.Node.Rpc.Client.IO.Observers;
-using Catalyst.Node.Rpc.Client.IO.Transport.Channels;
-using Catalyst.Protocol.Rpc.Node;
-using Microsoft.Extensions.Caching.Memory;
-using Multiformats.Hash.Algorithms;
-using Serilog;
+using Catalyst.Common.Types;
+using CommandLine;
 
 namespace Catalyst.Simulator
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
             Console.WriteLine("Catalyst Network Simulator");
 
-            var simulator = new Simulator();
+            var passwordRegistry = new PasswordRegistry();
+            Parser.Default.ParseArguments<Options>(args).WithParsed(options =>
+            {
+                if (!string.IsNullOrEmpty(options.NodePassword))
+                {
+                    AddPassword(passwordRegistry, options.NodePassword);
+                }
+
+                if (!string.IsNullOrEmpty(options.SslCertPassword))
+                {
+                    AddPassword(passwordRegistry, options.SslCertPassword);
+                }
+            });
+
+            var simulator = new Simulator(passwordRegistry);
             await simulator.Simulate();
 
-            //while (true)
-            //{
-            //    Thread.Sleep(100);
-            //}
+            return Environment.ExitCode;
+        }
+
+        private static void AddPassword(PasswordRegistry passwordRegistry, string password)
+        {
+            var secureString = new SecureString();
+            foreach (var character in password)
+            {
+                secureString.AppendChar(character);
+            }
+
+            passwordRegistry.AddItemToRegistry(PasswordRegistryTypes.CertificatePassword, secureString);
         }
     }
 }
--- src/Catalyst.Simulator/Simulator.cs
+++ src/Catalyst.Simulator/Simulator.cs
@@ -1,8 +1,33 @@
+#region LICENSE
+
+/**
+* Copyright (c) 2019 Catalyst Network
+*
+* This file is part of Catalyst.Node <https://github.com/catalyst-network/Catalyst.Node>
+*
+* Catalyst.Node is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Catalyst.Node is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Catalyst.Node. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using Catalyst.Common.Cryptography;
 using Catalyst.Common.Extensions;
 using Catalyst.Common.FileSystem;
+using Catalyst.Common.Interfaces.IO.Observers;
 using Catalyst.Common.Interfaces.P2P;
 using Catalyst.Common.Interfaces.Rpc;
 using Catalyst.Common.IO.EventLoop;
@@ -19,6 +44,8 @@ using Catalyst.Node.Rpc.Client;
 using Catalyst.Node.Rpc.Client.IO.Observers;
 using Catalyst.Node.Rpc.Client.IO.Transport.Channels;
 using Catalyst.Protocol.Rpc.Node;
+using Catalyst.Protocol.Transaction;
+using Google.Protobuf;
 using Microsoft.Extensions.Caching.Memory;
 using Multiformats.Hash.Algorithms;
 using Serilog;
@@ -27,6 +54,7 @@ namespace Catalyst.Simulator
 {
     public class Simulator
     {
+        private readonly ILogger _logger;
         private readonly FileSystem _fileSystem;
         private readonly ConsolePasswordReader _consolePasswordReader;
         private IPeerIdentifier _sender;
@@ -35,12 +63,11 @@ namespace Catalyst.Simulator
         private readonly NodeRpcClientFactory _nodeRpcClientFactory;
         private readonly ConsoleUserOutput _userOutput;
 
-        public Simulator()
+        public Simulator(PasswordRegistry passwordRegistry)
         {
+            _logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
             _fileSystem = new FileSystem();
             _userOutput = new ConsoleUserOutput();
-
-            var passwordRegistry = new PasswordRegistry();
             _consolePasswordReader = new ConsolePasswordReader(_userOutput, passwordRegistry);
 
             var wrapper = new CryptoWrapper();
@@ -48,28 +75,33 @@ namespace Catalyst.Simulator
 
             var keyServiceStore = new KeyStoreServiceWrapped(cryptoContext);
             var fileSystem = new FileSystem();
-            var logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            
             var multiHashAlgorithm = new BLAKE2B_256();
             var addressHelper = new AddressHelper(multiHashAlgorithm);
-            var localKeyStore = new LocalKeyStore(_consolePasswordReader, cryptoContext, keyServiceStore, fileSystem,
-                logger, addressHelper);
+            var localKeyStore = new LocalKeyStore(_consolePasswordReader, cryptoContext, keyServiceStore, fileSystem, _logger, addressHelper);
             var keyRegistry = new KeyRegistry();
             var keySigner = new KeySigner(localKeyStore, cryptoContext, keyRegistry);
 
             var memoryCacheOptions = new MemoryCacheOptions();
             var memoryCache = new MemoryCache(memoryCacheOptions);
             var changeTokenProvider = new TtlChangeTokenProvider(10000);
-            var messageCorrelationManager = new RpcMessageCorrelationManager(memoryCache, logger, changeTokenProvider);
+            var messageCorrelationManager = new RpcMessageCorrelationManager(memoryCache, _logger, changeTokenProvider);
             var peerIdValidator = new PeerIdValidator(cryptoContext);
             var nodeRpcClientChannelFactory =
                 new NodeRpcClientChannelFactory(keySigner, messageCorrelationManager, peerIdValidator);
 
-            var eventLoopGroupFactoryConfiguration = new EventLoopGroupFactoryConfiguration();
-            eventLoopGroupFactoryConfiguration.TcpClientHandlerWorkerThreads = 2;
+            var eventLoopGroupFactoryConfiguration = new EventLoopGroupFactoryConfiguration
+            {
+                TcpClientHandlerWorkerThreads = 2
+            };
+
             var tcpClientEventLoopGroupFactory = new TcpClientEventLoopGroupFactory(eventLoopGroupFactoryConfiguration);
 
-            var handlers = new[] {new GetVersionResponseObserver(logger)};
-            _nodeRpcClientFactory = new NodeRpcClientFactory(nodeRpcClientChannelFactory, tcpClientEventLoopGroupFactory, handlers);
+            var handlers = new List<IRpcResponseObserver>
+                {new BroadcastRawTransactionResponseObserver(_logger)};
+
+            _nodeRpcClientFactory =
+                new NodeRpcClientFactory(nodeRpcClientChannelFactory, tcpClientEventLoopGroupFactory, handlers);
         }
 
         public async Task Simulate()
@@ -100,20 +132,31 @@ namespace Catalyst.Simulator
             _recipient = PeerIdentifier.BuildPeerIdFromConfig(nodeRpcConfig);
 
             var dtoFactory = new DtoFactory();
-            _client.SubscribeToResponse<VersionResponse>(response =>
+
+            _client.SubscribeToResponse<BroadcastRawTransactionResponse>(response =>
             {
-                _userOutput.WriteLine("Received message");
-                var a = 0;
+                _userOutput.WriteLine($"Transaction response: {response.ResponseCode}");
             });
 
-            var messageDto = dtoFactory.GetDto(new VersionRequest().ToProtocolMessage(_sender.PeerId), _sender, _recipient);
-
+            var i = 0;
             await Task.Run(async () =>
             {
                 while (true)
                 {
-                    _userOutput.WriteLine("Sending message");
+                    var req = new BroadcastRawTransactionRequest();
+                    var transaction = new TransactionBroadcast();
+                    transaction.Signature = new TransactionSignature
+                    {
+                        SchnorrSignature = ByteString.CopyFromUtf8($"Signature{i}"),
+                        SchnorrComponent = ByteString.CopyFromUtf8($"Component{i}")
+                    };
+                    req.Transaction = transaction;
+
+                    var messageDto = dtoFactory.GetDto(req.ToProtocolMessage(_sender.PeerId), _sender, _recipient);
+
+                    _userOutput.WriteLine("Sending transaction");
                     _client.SendMessage(messageDto);
+                    i++;
                     await Task.Delay(500);
                 }
             });
