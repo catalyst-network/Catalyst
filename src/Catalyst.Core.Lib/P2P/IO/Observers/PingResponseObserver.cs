459bd063f23a1d9bcbc9ed4c648e5a5668ec759a
Sanjay Patel <sanjay.patel@atlascity.io>
2019-08-15 13:40:17 +0100

    Update PingResponseObserver.cs

--- src/Catalyst.Core.Lib/P2P/IO/Observers/PingResponseObserver.cs
+++ src/Catalyst.Core.Lib/P2P/IO/Observers/PingResponseObserver.cs
@@ -42,12 +42,15 @@ namespace Catalyst.Core.Lib.P2P.IO.Observers
             IP2PMessageObserver, IPeerClientObservable
     {
         private readonly IPeerChallenger _peerChallenger;
+        private readonly ILogger _logger;
+
         public ReplaySubject<IPeerClientMessageDto> ResponseMessageSubject { get; }
         public IObservable<IPeerClientMessageDto> MessageStream => ResponseMessageSubject.AsObservable();
 
         public PingResponseObserver(ILogger logger, IPeerChallenger peerChallenger)
             : base(logger)
         {
+            _logger = logger;
             _peerChallenger = peerChallenger;
             ResponseMessageSubject = new ReplaySubject<IPeerClientMessageDto>(1);
         }
@@ -57,6 +60,7 @@ namespace Catalyst.Core.Lib.P2P.IO.Observers
             IPeerIdentifier senderPeerIdentifier,
             ICorrelationId correlationId)
         {
+            _logger.Verbose("Received {0} from IP {1} Peer {2}", nameof(PingResponse), senderPeerIdentifier.Ip, senderPeerIdentifier.PeerId);
             ResponseMessageSubject.OnNext(new PeerClientMessageDto(pingResponse, senderPeerIdentifier, correlationId));
             _peerChallenger.ChallengeResponseMessageStreamer.OnNext(new PeerChallengerResponse(senderPeerIdentifier.PeerId));
         }
