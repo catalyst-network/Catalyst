eb3f9899ba67983566c8f44d948d17a69ec6a5a7
monsieurleberre <monsieur.leberre@gmail.com>
2019-08-19 16:01:04 +0100

    100% coverage for DeltaDfsReader

---
+++ src/Catalyst.Core.Lib.UnitTests/Modules/Consensus/Deltas/DeltaDfsReaderTests.cs
@@ -0,0 +1,100 @@
+#region LICENSE
+
+/**
+* Copyright (c) 2019 Catalyst Network
+*
+* This file is part of Catalyst.Node <https://github.com/catalyst-network/Catalyst.Node>
+*
+* Catalyst.Node is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Catalyst.Node is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Catalyst.Node. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System.IO;
+using System.Threading;
+using Catalyst.Common.Extensions;
+using Catalyst.Common.Interfaces.Modules.Dfs;
+using Catalyst.Core.Lib.Modules.Consensus.Deltas;
+using Catalyst.Protocol.Deltas;
+using Catalyst.TestUtils;
+using FluentAssertions;
+using Google.Protobuf;
+using MongoDB.Bson.Serialization.Serializers;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Serilog;
+using Xunit;
+
+namespace Catalyst.Core.Lib.UnitTests.Modules.Consensus.Deltas
+{
+    public class DeltaDfsReaderTests
+    {
+        private readonly IDfs _dfs;
+        private readonly ILogger _logger;
+        private readonly DeltaDfsReader _dfsReader;
+
+        public DeltaDfsReaderTests()
+        {
+            _dfs = Substitute.For<IDfs>();
+            _logger = Substitute.For<ILogger>();
+
+            _dfsReader = new DeltaDfsReader(_dfs, _logger);
+        }
+
+        [Fact]
+        public void TryReadDeltaFromDfs_Should_Return_False_And_Log_When_Hash_Not_Found_On_Dfs()
+        {
+            var exception = new FileNotFoundException("that hash is not good");
+            _dfs.ReadAsync(Arg.Any<string>(), Arg.Any<CancellationToken>())
+               .Throws(exception);
+
+            var badHash = "bad hash";
+            _dfsReader.TryReadDeltaFromDfs(badHash, out _, CancellationToken.None).Should().BeFalse();
+            _logger.Received(1).Error(exception,
+                Arg.Any<string>(),
+                Arg.Is<string>(s => s == badHash));
+        }
+
+        [Fact]
+        public void TryReadDeltaFromDfs_Should_Return_False_When_Hash_Not_Found_On_Dfs()
+        {
+            var goodHash = "good hash";
+            var matchingDelta = DeltaHelper.GetDelta();
+
+            _dfs.ReadAsync(goodHash, CancellationToken.None)
+               .Returns(matchingDelta.ToByteArray().ToMemoryStream());
+
+            var found = _dfsReader.TryReadDeltaFromDfs(goodHash, out var delta, CancellationToken.None);
+
+            found.Should().BeTrue();
+            delta.Should().Be(matchingDelta);
+        }
+
+        [Fact]
+        public void TryReadDeltaFromDfs_Should_Pass_Cancellation_Token()
+        {
+            var goodHash = "good hash";
+            var cancellationToken = new CancellationToken();
+
+            var matchingDelta = DeltaHelper.GetDelta();
+            _dfs.ReadAsync(goodHash, CancellationToken.None)
+               .Returns(matchingDelta.ToByteArray().ToMemoryStream());
+
+            var found = _dfsReader.TryReadDeltaFromDfs(goodHash, out var delta, CancellationToken.None);
+
+            _dfs.Received(1).ReadAsync(Arg.Is(goodHash), Arg.Is(cancellationToken));
+        }
+    }
+}
+
