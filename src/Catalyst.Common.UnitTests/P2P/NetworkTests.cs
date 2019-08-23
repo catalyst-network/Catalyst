278df731c2e4d5f0603d6fc8c62b404338f6efec
monsieurleberre <monsieur.leberre@gmail.com>
2019-08-23 16:48:14 +0100

    most important was public => private :)

--- src/Catalyst.Common.UnitTests/P2P/NetworkTests.cs
+++ src/Catalyst.Common.UnitTests/P2P/NetworkTests.cs
@@ -21,52 +21,35 @@
 
 #endregion
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using Catalyst.Common.Enumerator;
-using Catalyst.Protocol.Common;
 using FluentAssertions;
 using Xunit;
 
 namespace Catalyst.Common.UnitTests.P2P
 {
-    public static class NetworkTests
+    public class NetworkTests
     {
         static NetworkTests()
         {
-            NetworksAndExpectations = new []
-            {
-                Protocol.Common.Network.Devnet, 
-                Protocol.Common.Network.Mainnet, 
-                Protocol.Common.Network.Testnet
-            }.Select(n => new object[] {n.ToString(), n as object}).ToList();
+            NetworksAndExpectations = new[]
+                {
+                    Protocol.Common.Network.Devnet, 
+                    Protocol.Common.Network.Mainnet, 
+                    Protocol.Common.Network.Testnet
+                }.Select(n => new object[] {n.ToString(), n})
+               .ToList();
         }
 
-        private static List<object[]> NetworksAndExpectations { get; set; }
+        public static List<object[]> NetworksAndExpectations;
 
         [Theory]
         [MemberData(nameof(NetworksAndExpectations))]
-        public static void Network_can_be_parsed_from_string(string value, object expectedNetwork)
+        public static void Network_can_be_parsed_from_string(string value, Protocol.Common.Network expectedNetwork)
         {
-            Protocol.Common.Network.TryParse(value, out Protocol.Common.Network parsed);
+            Enum.TryParse(value, out Protocol.Common.Network parsed);
             parsed.Should().Be(expectedNetwork);
         }
-
-        /*[Fact]
-        public static void Network_can_be_parsed_from_string()
-        {
-            new List<Protocol.Common.Network>
-            {
-                Protocol.Common.Network.Devnet,
-                Protocol.Common.Network.Mainnet,
-                Protocol.Common.Network.Testnet
-            }.ForEach((x) =>
-            {
-                Protocol.Common.Network.TryParse(x.ToString(), out Protocol.Common.Network parsed);
-                parsed.Should().Be(x);
-            });
-        
-        }*/
-
     }
 }
