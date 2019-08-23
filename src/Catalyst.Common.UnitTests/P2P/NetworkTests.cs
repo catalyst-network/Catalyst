3b5c8d09d0ffbb84b7280792aadcc676e37bfb19
fran <fran.sl@atlascity.io>
2019-08-23 16:29:05 +0100

    test not working

--- src/Catalyst.Common.UnitTests/P2P/NetworkTests.cs
+++ src/Catalyst.Common.UnitTests/P2P/NetworkTests.cs
@@ -34,22 +34,39 @@ namespace Catalyst.Common.UnitTests.P2P
     {
         static NetworkTests()
         {
-            NetworksAndExpectations = new List<Protocol.Common.Network>
+            NetworksAndExpectations = new []
             {
                 Protocol.Common.Network.Devnet, 
                 Protocol.Common.Network.Mainnet, 
                 Protocol.Common.Network.Testnet
-            }.Select(n => new[] {n.ToString(), n as object}).ToList();
+            }.Select(n => new object[] {n.ToString(), n as object}).ToList();
         }
 
-        private static List<object[]> NetworksAndExpectations { get; }
+        private static List<object[]> NetworksAndExpectations { get; set; }
 
         [Theory]
         [MemberData(nameof(NetworksAndExpectations))]
-        public static void Network_can_be_parsed_from_string(string value, Protocol.Common.Network expectedNetwork)
+        public static void Network_can_be_parsed_from_string(string value, object expectedNetwork)
         {
             Protocol.Common.Network.TryParse(value, out Protocol.Common.Network parsed);
             parsed.Should().Be(expectedNetwork);
         }
+
+        /*[Fact]
+        public static void Network_can_be_parsed_from_string()
+        {
+            new List<Protocol.Common.Network>
+            {
+                Protocol.Common.Network.Devnet,
+                Protocol.Common.Network.Mainnet,
+                Protocol.Common.Network.Testnet
+            }.ForEach((x) =>
+            {
+                Protocol.Common.Network.TryParse(x.ToString(), out Protocol.Common.Network parsed);
+                parsed.Should().Be(x);
+            });
+        
+        }*/
+
     }
 }
