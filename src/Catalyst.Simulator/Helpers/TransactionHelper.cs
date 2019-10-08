5911b5032e4963eab90898b0260f89ac32419cd3
monsieurleberre <monsieurleberre@users.noreply.github.com>
2019-10-08 14:26:22 +0100

    update proto submodule to add transactoin nonce and remove rangeProof complexity (#1034)
    * update proto submodule to add transaction nonce and remove range proof complexity

--- src/Catalyst.Simulator/Helpers/TransactionHelper.cs
+++ src/Catalyst.Simulator/Helpers/TransactionHelper.cs
@@ -44,7 +44,7 @@ namespace Catalyst.Simulator.Helpers
             SignatureType = SignatureType.TransactionPublic
         };
 
-        public static BroadcastRawTransactionRequest GenerateTransaction(uint amount, int fee)
+        public static BroadcastRawTransactionRequest GenerateTransaction(uint amount, int fee, int nonce = 0)
         {
             var cryptoWrapper = new FfiWrapper();
             var privateKey = cryptoWrapper.GeneratePrivateKey();
@@ -59,6 +59,7 @@ namespace Catalyst.Simulator.Helpers
                         Amount = ((UInt256) amount).ToUint256ByteString(),
                         Base = new BaseEntry
                         {
+                            Nonce = (ulong) nonce,
                             SenderPublicKey = privateKey.GetPublicKey().Bytes.ToByteString(),
                             ReceiverPublicKey = publicKey,
                             TransactionFees = ((UInt256) fee).ToUint256ByteString()
