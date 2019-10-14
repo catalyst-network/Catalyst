45634e14624b40ddc694baa95bcd183df4c6d37e
CAT-OPS <catops@catalystnet.org>
2019-10-14 21:34:09 +0000

    publish gh-pages

--- docs/api/index.md
+++
@@ -1,1 +0,0 @@
-﻿# Catalyst Framework Api
--- scripts/addLicenseHeader.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in $(find ../src -name '*.cs')
-do
-  if ! grep -q region LICENSE $i
-  then
-    cat COPYING $i >$i.new && mv $i.new $i
-  fi
-done
