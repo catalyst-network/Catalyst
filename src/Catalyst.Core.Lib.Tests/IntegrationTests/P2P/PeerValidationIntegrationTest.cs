a4d30438ccf2d2377f31aab1352198fab92bceb3
NshCore <nshcore@protonmail.com>
2019-10-23 02:03:16 +0100

    wip

--- src/Catalyst.Core.Lib.Tests/IntegrationTests/P2P/PeerValidationIntegrationTest.cs
+++ src/Catalyst.Core.Lib.Tests/IntegrationTests/P2P/PeerValidationIntegrationTest.cs
@@ -104,7 +104,7 @@ namespace Catalyst.Core.Lib.Tests.IntegrationTests.P2P
                     keySigner,
                     ContainerProvider.Container.Resolve<IPeerIdValidator>(),
                     ContainerProvider.Container.Resolve<IPeerSettings>()),
-                new DiscoveryHelper.DevDiscover(), 
+                Substitute.For<IPeerDiscovery>(), 
                 ContainerProvider.Container.Resolve<IEnumerable<IP2PMessageObserver>>(),
                 _peerSettings,
                 ContainerProvider.Container.Resolve<ILogger>(),
@@ -113,16 +113,16 @@ namespace Catalyst.Core.Lib.Tests.IntegrationTests.P2P
             await _peerService.StartAsync();
         }
 
-        [Fact(Skip = "false")]
-        [Trait(Traits.TestType, Traits.IntegrationTest)]
-        public async Task PeerChallenge_PeerIdentifiers_Expect_To_Succeed_Valid_IP_Port_PublicKey()
-        {
-            await Setup().ConfigureAwait(false);
-            var valid = await RunPeerChallengeTask(_peerSettings.PublicKey, _peerSettings.BindAddress,
-                _peerSettings.Port).ConfigureAwait(false);
-
-            valid.Should().BeTrue();
-        }
+        // [Fact(Skip = "true")]
+        // [Trait(Traits.TestType, Traits.IntegrationTest)]
+        // public async Task PeerChallenge_PeerIdentifiers_Expect_To_Succeed_Valid_IP_Port_PublicKey()
+        // {
+        //     await Setup().ConfigureAwait(false);
+        //     var valid = await RunPeerChallengeTask(_peerSettings.PublicKey, _peerSettings.BindAddress,
+        //         _peerSettings.Port).ConfigureAwait(false);
+        //
+        //     valid.Should().BeTrue();
+        // }
 
         [Theory]
         [Trait(Traits.TestType, Traits.IntegrationTest)]
