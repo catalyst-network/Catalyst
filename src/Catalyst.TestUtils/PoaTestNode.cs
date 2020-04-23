292ddecd2e60c924fc7ca78db5c6f22496af9f15
fran <fran.sl@atlascity.io>
2020-04-23 20:28:38 +0100

    add method

--- src/Catalyst.TestUtils/PoaTestNode.cs
+++ src/Catalyst.TestUtils/PoaTestNode.cs
@@ -28,11 +28,15 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Autofac;
+using Autofac.Core;
 using Catalyst.Abstractions;
+using Catalyst.Abstractions.Cli;
 using Catalyst.Abstractions.Consensus;
 using Catalyst.Abstractions.Cryptography;
+using Catalyst.Abstractions.DAO;
 using Catalyst.Abstractions.Dfs;
 using Catalyst.Abstractions.FileSystem;
+using Catalyst.Abstractions.IO.Observers;
 using Catalyst.Abstractions.Keystore;
 using Catalyst.Abstractions.Ledger.Models;
 using Catalyst.Abstractions.Mempool;
@@ -44,6 +48,9 @@ using Catalyst.Core.Lib.Config;
 using Catalyst.Core.Lib.DAO.Transaction;
 using Catalyst.Core.Lib.P2P.Models;
 using Catalyst.Abstractions.P2P.Repository;
+using Catalyst.Core.Lib;
+using Catalyst.Core.Lib.Cli;
+using Catalyst.Core.Lib.DAO;
 using Catalyst.Core.Modules.Ledger.Repository;
 using Catalyst.Core.Modules.Mempool;
 using Catalyst.Core.Modules.Mempool.Repositories;
@@ -55,13 +62,27 @@ using SharpRepository.InMemoryRepository;
 using Catalyst.Core.Lib.P2P.Repository;
 using Nethermind.Db;
 using Catalyst.Core.Lib.DAO.Ledger;
+using Catalyst.Core.Modules.Authentication;
+using Catalyst.Core.Modules.Consensus;
+using Catalyst.Core.Modules.Cryptography.BulletProofs;
+using Catalyst.Core.Modules.Dfs;
+using Catalyst.Core.Modules.Hashing;
+using Catalyst.Core.Modules.KeySigner;
+using Catalyst.Core.Modules.Keystore;
+using Catalyst.Core.Modules.Kvm;
+using Catalyst.Core.Modules.Ledger;
+using Catalyst.Core.Modules.P2P.Discovery.Hastings;
 using Catalyst.Core.Modules.Rpc.Server;
+using Catalyst.Core.Modules.Sync;
 using Catalyst.Core.Modules.Web3;
+using Catalyst.Modules.POA.Consensus;
+using Catalyst.Modules.POA.P2P;
+using Catalyst.Node.POA.CE;
 using SharpRepository.Repository;
 
 namespace Catalyst.TestUtils
 {
-    public class PoaTestNode : ICatalystNode, IDisposable
+    public sealed class PoaTestNode : ICatalystNode, IDisposable
     {
         private readonly IDfsService _dfsService;
         private readonly IMempool<PublicEntryDao> _memPool;
@@ -72,7 +93,7 @@ namespace Catalyst.TestUtils
         private readonly IPeerRepository _peerRepository;
         private readonly IRpcServerSettings _rpcSettings;
         private readonly ILifetimeScope _scope;
-        public readonly ContainerProvider ContainerProvider;
+        private readonly ContainerProvider _containerProvider;
         private readonly IDeltaByNumberRepository _deltaByNumber;
 
         public PoaTestNode(string name,
@@ -104,24 +125,24 @@ namespace Catalyst.TestUtils
 
             _deltaByNumber = new DeltaByNumberRepository(new InMemoryRepository<DeltaByNumber, string>());
 
-            ContainerProvider = new ContainerProvider(new[]
+            _containerProvider = new ContainerProvider(new[]
                 {
                     Constants.NetworkConfigFile(NetworkType.Devnet),
                     Constants.SerilogJsonConfigFile
                 }
                .Select(f => Path.Combine(Constants.ConfigSubFolder, f)), parentTestFileSystem, TestContext.CurrentContext);
 
-            Program.RegisterNodeDependencies(ContainerProvider.ContainerBuilder,
+            RegisterNodeDependencies(_containerProvider.ContainerBuilder,
                 excludedModules: new List<Type>
                 {
                     typeof(ApiModule),
                     typeof(RpcServerModule)
                 }
             );
-            ContainerProvider.ConfigureContainerBuilder(true, true);
+            _containerProvider.ConfigureContainerBuilder(true, true);
             OverrideContainerBuilderRegistrations();
 
-            _scope = ContainerProvider.Container.BeginLifetimeScope(Name);
+            _scope = _containerProvider.Container.BeginLifetimeScope(Name);
             _node = _scope.Resolve<ICatalystNode>();
 
             var keyStore = _scope.Resolve<IKeyStore>();
@@ -133,8 +154,67 @@ namespace Catalyst.TestUtils
                .ConfigureAwait(false).GetAwaiter()
                .GetResult();
         }
+        
+        public static void RegisterNodeDependencies(ContainerBuilder containerBuilder,
+            List<IModule> extraModuleInstances = default,
+            List<Type> excludedModules = default)
+        {
+            // core modules
+            containerBuilder.RegisterType<CatalystNodePoa>().As<ICatalystNode>();
+            containerBuilder.RegisterType<ConsoleUserOutput>().As<IUserOutput>();
+            containerBuilder.RegisterType<ConsoleUserInput>().As<IUserInput>();
 
-        public string Name { get; }
+            // message handlers
+            containerBuilder.RegisterAssemblyTypes(typeof(CoreLibProvider).Assembly)
+                .AssignableTo<IP2PMessageObserver>().As<IP2PMessageObserver>();
+
+            containerBuilder.RegisterAssemblyTypes(typeof(RpcServerModule).Assembly)
+                .AssignableTo<IRpcRequestObserver>().As<IRpcRequestObserver>()
+                .PublicOnly();
+
+            // DAO MapperInitialisers
+            containerBuilder.RegisterAssemblyTypes(typeof(CoreLibProvider).Assembly)
+                .AssignableTo<IMapperInitializer>().As<IMapperInitializer>();
+            containerBuilder.RegisterType<MapperProvider>().As<IMapperProvider>()
+                .SingleInstance();
+
+            var modulesToRegister = DefaultModulesByTypes
+                .Where(p => excludedModules == null || !excludedModules.Contains(p.Key))
+                .Select(p => p.Value())
+                .Concat(extraModuleInstances ?? new List<IModule>());
+
+            foreach (var module in modulesToRegister)
+            {
+                containerBuilder.RegisterModule(module);
+            }
+        }
+        
+        private static readonly Dictionary<Type, Func<IModule>> DefaultModulesByTypes =
+            new Dictionary<Type, Func<IModule>>
+            {
+                {typeof(CoreLibProvider), () => new CoreLibProvider()},
+                {typeof(MempoolModule), () => new MempoolModule()},
+                {typeof(ConsensusModule), () => new ConsensusModule()},
+                {typeof(SynchroniserModule), () => new SynchroniserModule()},
+                {typeof(KvmModule), () => new KvmModule()},
+                {typeof(LedgerModule), () => new LedgerModule()},
+                {typeof(HashingModule), () => new HashingModule()},
+                {typeof(DiscoveryHastingModule), () => new DiscoveryHastingModule()},
+                {typeof(RpcServerModule), () => new RpcServerModule()},
+                {typeof(BulletProofsModule), () => new BulletProofsModule()},
+                {typeof(KeystoreModule), () => new KeystoreModule()},
+                {typeof(KeySignerModule), () => new KeySignerModule()},
+                {typeof(DfsModule), () => new DfsModule()},
+                {typeof(AuthenticationModule), () => new AuthenticationModule()},
+                {
+                    typeof(ApiModule),
+                    () => new ApiModule("http://*:5005", new List<string> {"Catalyst.Core.Modules.Web3", "Catalyst.Core.Modules.Dfs"})
+                },
+                {typeof(PoaConsensusModule), () => new PoaConsensusModule()},
+                {typeof(PoaP2PModule), () => new PoaP2PModule()}
+            };
+
+        private string Name { get; }
 
         public IConsensus Consensus => _node.Consensus;
 
@@ -147,9 +227,9 @@ namespace Catalyst.TestUtils
 
         public void Dispose() { Dispose(true); }
 
-        protected void OverrideContainerBuilderRegistrations()
+        private void OverrideContainerBuilderRegistrations()
         {
-            var builder = ContainerProvider.ContainerBuilder;
+            var builder = _containerProvider.ContainerBuilder;
 
             builder.RegisterInstance(_deltaByNumber).As<IDeltaByNumberRepository>();
             builder.RegisterInstance(new MemDb()).As<IDb>().SingleInstance();
@@ -161,19 +241,19 @@ namespace Catalyst.TestUtils
             builder.RegisterInstance(new InMemoryRepository<TransactionToDelta, string>())
                .AsImplementedInterfaces();
 
-            ContainerProvider.ContainerBuilder.RegisterInstance(new TestPasswordReader()).As<IPasswordReader>();
-            ContainerProvider.ContainerBuilder.RegisterInstance(_nodeSettings).As<IPeerSettings>();
-            ContainerProvider.ContainerBuilder.RegisterInstance(_rpcSettings).As<IRpcServerSettings>();
-            ContainerProvider.ContainerBuilder.RegisterInstance(_nodePeerId).As<PeerId>();
-            ContainerProvider.ContainerBuilder.RegisterInstance(_memPool).As<IMempool<PublicEntryDao>>();
-            ContainerProvider.ContainerBuilder.RegisterInstance(_dfsService).As<IDfsService>();
-            ContainerProvider.ContainerBuilder.RegisterInstance(_peerRepository).As<IPeerRepository>();
-            ContainerProvider.ContainerBuilder.RegisterType<TestFileSystem>().As<IFileSystem>()
+            _containerProvider.ContainerBuilder.RegisterInstance(new TestPasswordReader()).As<IPasswordReader>();
+            _containerProvider.ContainerBuilder.RegisterInstance(_nodeSettings).As<IPeerSettings>();
+            _containerProvider.ContainerBuilder.RegisterInstance(_rpcSettings).As<IRpcServerSettings>();
+            _containerProvider.ContainerBuilder.RegisterInstance(_nodePeerId).As<PeerId>();
+            _containerProvider.ContainerBuilder.RegisterInstance(_memPool).As<IMempool<PublicEntryDao>>();
+            _containerProvider.ContainerBuilder.RegisterInstance(_dfsService).As<IDfsService>();
+            _containerProvider.ContainerBuilder.RegisterInstance(_peerRepository).As<IPeerRepository>();
+            _containerProvider.ContainerBuilder.RegisterType<TestFileSystem>().As<IFileSystem>()
                .WithParameter("rootPath", _nodeDirectory.FullName);
-            ContainerProvider.ContainerBuilder.RegisterInstance(Substitute.For<IPeerDiscovery>()).As<IPeerDiscovery>();
+            _containerProvider.ContainerBuilder.RegisterInstance(Substitute.For<IPeerDiscovery>()).As<IPeerDiscovery>();
         }
 
-        protected virtual void Dispose(bool disposing)
+        private void Dispose(bool disposing)
         {
             if (!disposing)
             {
@@ -182,7 +262,7 @@ namespace Catalyst.TestUtils
 
             _scope?.Dispose();
             _peerRepository?.Dispose();
-            ContainerProvider?.Dispose();
+            _containerProvider?.Dispose();
         }
     }
 }
